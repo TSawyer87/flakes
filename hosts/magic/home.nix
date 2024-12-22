6b75c99a5e51de4d9c43d3792117dcf6077750f1
installer <installer@gmail.com>
2024-12-22 10:34:40 -0500

    foot foot --server

--- hosts/magic/home.nix
+++ hosts/magic/home.nix
@@ -94,6 +94,21 @@ in
     ];
   };
 
+  programs.foot = {
+    enable = true;
+    server.enable = true;
+    settings = {
+       main = {
+        term = "xterm-256color";
+        font = "JetBrains Mono:size=12";
+        dpi-aware = "yes";
+    };
+      mouse = {
+        hide-when-typing = "yes";
+      };
+    };
+  };
+
   programs.wezterm = {
     enable = true;
     package = inputs.wezterm.packages.${system}.default;
