e58e4b92ed5918debcd6ca99fe42d16cd2633895
installer <installer@gmail.com>
2024-12-22 10:19:29 -0500

    remove basically everything from hyprlock

--- hosts/magic/home.nix
+++ hosts/magic/home.nix
@@ -299,21 +299,21 @@ in
         #     valign = "center";
         #   }
         # ];
-        input-field = [
-          {
-            size = "200, 50";
-            position = "0, -80";
-            monitor = "";
-            dots_center = true;
-            fade_on_empty = false;
-            font_color = "rgb(CFE6F4)";
-            inner_color = "rgb(657DC2)";
-            outer_color = "rgb(0D0E15)";
-            outline_thickness = 5;
-            placeholder_text = "Password...";
-            shadow_passes = 2;
-          }
-        ];
+        # input-field = [
+        #   {
+        #     size = "200, 50";
+        #     position = "0, -80";
+        #     monitor = "";
+        #     dots_center = true;
+        #     fade_on_empty = false;
+        #     font_color = "rgb(CFE6F4)";
+        #     inner_color = "rgb(657DC2)";
+        #     outer_color = "rgb(0D0E15)";
+        #     outline_thickness = 5;
+        #     placeholder_text = "Password...";
+        #     shadow_passes = 2;
+        #   }
+        # ];
       };
     };
 
