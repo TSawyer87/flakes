4e7a16adb957fc0b66b57e7f93157615d3383fd2
installer <installer@gmail.com>
2024-12-22 09:47:43 -0500

    modular bash

---
+++ config/bash.nix
@@ -0,0 +1,86 @@
+{ pkgs, host, username, ... }:
+
+    programs.bash = {
+      enable = true;
+      enableCompletion = true;
+      profileExtra = ''
+        #if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+        #  exec Hyprland
+        #fi
+      '';
+      bashrcExtra = ''
+        export PATH=$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin
+        export MANPAGER='nvim +Man!'
+      '';
+      initExtra = ''
+        fastfetch
+        if [ -f $HOME/.bashrc-personal ]; then
+          source $HOME/.bashrc-personal
+        fi
+        shopt -s autocd
+        eval "$(zoxide init bash)"
+        eval "$(mcfly init bash)"
+      '';
+      shellAliases = {
+        sv = "sudo nvim";
+        fr = "nh os switch --hostname ${host} /home/${username}/flakes";
+        fu = "nh os switch --hostname ${host} --update /home/${username}/flakes";
+        ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+        v = "nvim";
+        cat = "bat";
+        ls = "eza --icons";
+        ll = "eza -lh --icons --grid --group-directories-first";
+        la = "eza -lah --icons --grid --group-directories-first";
+        yz = "yazi";
+        ".." = "cd ..";
+        "..." = "cd ../..";
+        "...." = "cd ../../../";
+      };
+    
+    home-manager.enable = true;
+    hyprlock = {
+      enable = false;
+      settings = {
+        general = {
+          disable_loading_bar = true;
+          grace = 10;
+          hide_cursor = true;
+          no_fade_in = false;
+        };
+        background = [
+          {
+            path = "/home/${username}/Pictures/Wallpapers/Wall.png";
+            blur_passes = 3;
+            blur_size = 8;
+          }
+        ];
+        image = [
+          {
+            path = "/home/${username}/.config/5-cm.jpg";
+            size = 150;
+            border_size = 4;
+            border_color = "rgb(0C96F9)";
+            rounding = -1; # Negative means circle
+            position = "0, 200";
+            halign = "center";
+            valign = "center";
+          }
+        ];
+        input-field = [
+          {
+            size = "200, 50";
+            position = "0, -80";
+            monitor = "";
+            dots_center = true;
+            fade_on_empty = false;
+            font_color = "rgb(CFE6F4)";
+            inner_color = "rgb(657DC2)";
+            outer_color = "rgb(0D0E15)";
+            outline_thickness = 5;
+            placeholder_text = "Password...";
+            shadow_passes = 2;
+          }
+        ];
+      };
+    };
+}
