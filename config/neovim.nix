d84d201d23920b2706fb5b83b104a7ffb2a54c4b
installer <installer@gmail.com>
2024-12-23 07:13:46 -0500

    lualine

--- config/neovim.nix
+++ config/neovim.nix
@@ -112,16 +112,13 @@ in
         ${builtins.readFile ./nvim/plugins/ufo.lua}
         ${builtins.readFile ./nvim/plugins/flash.lua}
         ${builtins.readFile ./nvim/plugins/toggleterm.lua}
+        ${builtins.readFile ./nvim/plugins/lualine.lua}
         ${builtins.readFile ./nvim/plugins/which-key.lua}
         ${builtins.readFile ./.stylua.toml}
         require("wezterm").setup{}
         require("render-markdown").setup{}
         require("ibl").setup()
         require("bufferline").setup{}
-        require("lualine").setup({
-          icons_enabled = true,
-          theme = "tokyonight",
-        })
       '';
     };
   };
