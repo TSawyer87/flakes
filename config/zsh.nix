2dedf40b5433704237356a54738cb57056689807
installer <installer@gmail.com>
2024-12-23 17:30:43 -0500

    zsh-nix

--- config/zsh.nix
+++ config/zsh.nix
@@ -22,7 +22,7 @@
         src = pkgs.fetchFromGitHub {
           owner = "chisui";
           repo = "zsh-nix-shell";
-          rev = "v0.8.0";  # Check for the latest version.
+          rev = "v0.8.0";
           sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
         };
       }
