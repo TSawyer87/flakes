bf360de7cce15a38d775ab5bf28e9b53e98113de
installer <installer@gmail.com>
2024-12-23 17:31:39 -0500

    zsh-nix

--- config/zsh.nix
+++ config/zsh.nix
@@ -23,7 +23,7 @@
           owner = "chisui";
           repo = "zsh-nix-shell";
           rev = "v0.8.0";
-          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          # sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
         };
       }
       ];
