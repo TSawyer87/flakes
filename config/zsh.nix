a67f082bb3bc640ca1b35eb2cff0080a6e52dc97
installer <installer@gmail.com>
2024-12-24 06:36:22 -0500

    zsh

--- config/zsh.nix
+++ config/zsh.nix
@@ -1,9 +1,4 @@
-{
-  pkgs,
-  host,
-  username,
-  ...
-}:
+{ pkgs, host, username, ... }:
 
 {
   programs.zsh = {
@@ -13,20 +8,12 @@
     syntaxHighlighting.enable = true;
     oh-my-zsh = {
       enable = true;
-      plugins = [
-        "git"
-        "sudo"
-      ];
+      plugins = [ "git" "sudo" "nix-shell-completions" ];
     };
     profileExtra = ''
       #if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
       #  exec Hyprland
       #fi
-      export MCFLY_KEY_SCHEME=vim
-      export MCFLY_FUZZY=2
-      export MCFLY_RESULTS=50
-      export MCFLY_RESULTS_SORT=LAST_RUN
-      export MCFLY_INTERFACE_VIEW=BOTTOM
       setopt correct                                                  # Auto correct mistakes
       setopt extendedglob                                             # Extended globbing. Allows using regular expressions with *
       setopt nocaseglob                                               # Case insensitive globbing
@@ -40,37 +27,45 @@
       setopt auto_pushd
       setopt pushd_ignore_dups
       setopt pushdminus
+      prompt_nix_shell_setup
     '';
     initExtra = ''
-      fastfetch
-      if [ -f $HOME/.zshrc-personal ]; then
-        source $HOME/.zshrc-personal
-      fi
-      source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
-      eval "$(zoxide init zsh)"
-      eval "$(mcfly init zsh)"
-      export MANPAGER='nvim +Man!'
-      # fzf
-      export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git"'
-      export FZF_CTRL_T_OPTS="
-      --walker-skip .git,node_modules,target
-      --preview 'bat -n --color=always {}'
-      --bind 'ctrl-/:change-preview-window(down|hidden|)'"
-      export FZF_DEFAULT_OPTS="--height 60% \
-      --border sharp \
-      --layout reverse \
-      --color '$FZF_COLORS' \
-      --prompt '∷ ' \
-      --pointer ▶ \
-      --marker ⇒"
-      export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -n 10'"
-      export FZF_COMPLETION_DIR_COMMANDS="cd pushd rmdir tree ls"
+            fastfetch
+            if [ -f $HOME/.zshrc-personal ]; then
+              source $HOME/.zshrc-personal
+            fi
+            source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+            source ${pkgs.zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh
+            eval "$(zoxide init zsh)"
+            eval "$(mcfly init zsh)"
+            export MANPAGER='nvim +Man!'
+            export MCFLY_KEY_SCHEME=vim
+            export MCFLY_FUZZY=2
+            export MCFLY_RESULTS=50
+            export MCFLY_RESULTS_SORT=LAST_RUN
+            export MCFLY_INTERFACE_VIEW=BOTTOM
+            # fzf
+            export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git"'
+            export FZF_CTRL_T_OPTS="
+            --walker-skip .git,node_modules,target
+            --preview 'bat -n --color=always {}'
+            --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+            export FZF_DEFAULT_OPTS="--height 60% \
+            --border sharp \
+            --layout reverse \
+            --color '$FZF_COLORS' \
+            --prompt '∷ ' \
+            --pointer ▶ \
+            --marker ⇒"
+            export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -n 10'"
+            export FZF_COMPLETION_DIR_COMMANDS="cd pushd rmdir tree ls"
     '';
     shellAliases = {
       sv = "sudo nvim";
       fr = "nh os switch --hostname ${host} /home/${username}/flakes";
       fu = "nh os switch --hostname ${host} --update /home/${username}/flakes";
-      ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+      ncg =
+        "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       opts = "man home-configuration.nix";
       zed = "zeditor";
       lg = "lazygit";
