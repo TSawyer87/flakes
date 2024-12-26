c83ecd571833ac7199e0c7506529e5e6b8430583
TSawyer87 <sawyerjr.25@gmail.com>
2024-12-26 13:52:56 -0500

    stylix pic change

--- hosts/laptop/config.nix
+++ hosts/laptop/config.nix
@@ -48,8 +48,9 @@ in {
     # image = ../../config/wallpapers/Wallpaper.png;
     # image = ../../config/wallpapers/gruvbox-dark-japanese-street.jpg;
     # image = ../../config/wallpapers/5-cm.jpg;
-    image = ../../config/wallpapers/nightTab_backdrop.jpg;
+    # image = ../../config/wallpapers/nightTab_backdrop.jpg;
     # image = ../../config/wallpapers/original-anime-cafe.jpg;
+     image = ../../config/wallpapers/Lofi-Cafe1.png;
     # base16Scheme = {
     #   base00 = "232136";
     #   base01 = "2a273f";
