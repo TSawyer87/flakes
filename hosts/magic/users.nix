99b916a50e35fb5da8737bba8b8027588e15c07f
installer <installer@gmail.com>
2024-12-26 11:06:48 -0500

    add hashedPassword

--- hosts/magic/users.nix
+++ hosts/magic/users.nix
@@ -1,25 +1,15 @@
-{
-  pkgs,
-  username,
-  ...
-}:
+{ pkgs, username, ... }:
 
-let
-  inherit (import ./variables.nix) gitUsername;
-in
-{
+let inherit (import ./variables.nix) gitUsername;
+in {
   users.users = {
     "${username}" = {
       homeMode = "755";
       isNormalUser = true;
       description = "${gitUsername}";
-      extraGroups = [
-        "networkmanager"
-        "wheel"
-        "libvirtd"
-        "scanner"
-        "lp"
-      ];
+      hashedPassword =
+        "$6$hLxz1nh01PVcUQ6e$4o6tYrRxbRQQFRN3NSUMkPuwdRpOhNdp1s07TAYr2shcbdQUkYurHyk8Xp8FvjVPwr60N4NSPDmwUr6Nd5FD9.";
+      extraGroups = [ "networkmanager" "wheel" "libvirtd" "scanner" "lp" ];
       shell = pkgs.zsh;
       ignoreShellProgramCheck = true;
       packages = with pkgs; [
