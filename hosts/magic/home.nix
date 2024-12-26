cac732dde812b552a83dfb8f6d6ed470f16e73b9
installer <installer@gmail.com>
2024-12-26 09:28:51 -0500

    nix-direnv

--- hosts/magic/home.nix
+++ hosts/magic/home.nix
@@ -100,6 +100,16 @@ in {
     };
   };
 
+  programs = {
+    direnv = {
+      enable = true;
+      enableBashIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+
+    bash.enable = true; # see note on other shells below
+  };
+
   # Create XDG Dirs
   xdg = {
     configFile."mimeapps.list".text = ''
