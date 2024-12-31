c91614c8d708f88c06d7e375484d0c9f236c7ee1
TSawyer87 <sawyerjr.25@gmail.com>
2024-12-31 07:37:19 -0500

    starship

--- hosts/magic/home.nix
+++ hosts/magic/home.nix
@@ -24,6 +24,7 @@ in {
     ../../config/wezterm.nix
     ../../config/kitty.nix
     ../../config/zed.nix
+    ../../config/starship.nix
   ];
 
   # Place Files Inside Home Directory
@@ -215,66 +216,6 @@ in {
     btop = {
       enable = true;
       settings = { vim_keys = true; };
-    };
-    starship = {
-      enable = true;
-      package = pkgs.starship;
-      add_newline = true;
-      settings = {
-        format =
-          "$shlvl$shell$username$hostname$nix_shell$git_branch$git_commit$git_state$git_status$directory$jobs$cmd_duration$character";
-        shlvl = {
-          disabled = false;
-          symbol = "ﰬ";
-          style = "bright-red bold";
-        };
-        shell = {
-          disabled = false;
-          format = "$indicator";
-          fish_indicator = "";
-          bash_indicator = "[BASH](bright-white) ";
-          zsh_indicator = "[ZSH](bright-white) ";
-        };
-        username = {
-          style_user = "bright-white bold";
-          style_root = "bright-red bold";
-        };
-        hostname = {
-          style = "bright-green bold";
-          ssh_only = true;
-        };
-        nix_shell = {
-          symbol = "";
-          format = "[$symbol$name]($style) ";
-          style = "bright-purple bold";
-        };
-        git_branch = {
-          only_attached = true;
-          format = "[$symbol$branch]($style) ";
-          symbol = "שׂ";
-          style = "bright-yellow bold";
-        };
-        git_commit = {
-          only_detached = true;
-          format = "[ﰖ$hash]($style) ";
-          style = "bright-yellow bold";
-        };
-        git_state = { style = "bright-purple bold"; };
-        git_status = { style = "bright-green bold"; };
-        directory = {
-          read_only = " ";
-          truncation_length = 0;
-        };
-        cmd_duration = {
-          format = "[$duration]($style) ";
-          style = "bright-blue";
-        };
-        jobs = { style = "bright-green bold"; };
-        character = {
-          success_symbol = "[\\$](bright-green bold)";
-          error_symbol = "[\\$](bright-red bold)";
-        };
-      };
     };
     home-manager.enable = true;
     hyprlock = {
