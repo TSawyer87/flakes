6cf36cc73913fbce974caf0f901cfe03f962934d
TSawyer87 <sawyerjr.25@gmail.com>
2024-12-31 07:44:30 -0500

    hyprlock

--- hosts/magic/home.nix
+++ hosts/magic/home.nix
@@ -232,16 +232,16 @@ in {
           # blur_passes = 3;
           # blur_size = 8;
         # }];
-        image = [{
-          path = "/home/${username}/.config/5-cm.jpg";
-          size = 150;
-          border_size = 4;
-          border_color = "rgb(0C96F9)";
-          rounding = -1; # Negative means circle
-          position = "0, 200";
-          halign = "center";
-          valign = "center";
-        }];
+        # image = [{
+        #   path = "/home/${username}/.config/5-cm.jpg";
+        #   size = 150;
+        #   border_size = 4;
+        #   border_color = "rgb(0C96F9)";
+        #   rounding = -1; # Negative means circle
+        #   position = "0, 200";
+        #   halign = "center";
+        #   valign = "center";
+        # }];
         input-field = [{
           size = "200, 50";
           position = "0, -80";
