650391685f16d250dcf7ae7aa112e20167c2ddd6
TSawyer87 <sawyerjr.25@gmail.com>
2024-12-31 07:18:27 -0500

    starship

--- hosts/magic/home.nix
+++ hosts/magic/home.nix
@@ -220,197 +220,153 @@ in {
       enable = true;
       package = pkgs.starship;
       settings = {
-      # FIRST LINE/ROW: Info & Status
-      username = {
-        format = " [╭─$user]($style)@";
-        show_always = true;
-        style_root = "bold red";
-        style_user = "bold red";
-      };
-      hostname = {
-        disabled = false;
-        format = "[$hostname]($style) in ";
-        ssh_only = false;
-        style = "bold dimmed red";
-        trim_at = "-";
-      };
-      directory = {
-        style = "purple";
-        truncate_to_repo = true;
-        truncation_length = 0;
-        truncation_symbol = "repo: ";
-      };
-      sudo = {
-        disabled = false;
-      };
-      git_status = {
-        ahead = "⇡${count}";
-        behind = "⇣${count}";
-        deleted = "x";
-        diverged = "⇕⇡${ahead_count}⇣${behind_count}";
-        style = "white";
-      };
-      cmd_duration = {
-        disabled = false;
-        format = "took [$duration]($style)";
-        min_time = 1;
-      };
-
-      # SECOND LINE/ROW: Prompt
-      battery = {
-        charging_symbol = "";
-        disabled = true;
-        discharging_symbol = "";
-        full_symbol = "";
-        display = [
-          {
-            disabled = false;
-            style = "bold red";
-            threshold = 15;
-          }
-          {
-            disabled = true;
-            style = "bold yellow";
-            threshold = 50;
-          }
-          {
-            disabled = true;
-            style = "bold green";
-            threshold = 80;
-          }
-        ];
-      };
-      time = {
-        disabled = true;
-        format = " 🕙 $time($style)\n";
-        style = "bright-white";
-        time_format = "%T";
-      };
-      character = {
-        error_symbol = " [×](bold red)";
-        success_symbol = " [╰─λ](bold red)";
-      };
-
-      # SYMBOLS
-      status = {
-        disabled = false;
-        format = "[\[$symbol$status_common_meaning$status_signal_name$status_maybe_int\]]($style)";
-        map_symbol = true;
-        pipestatus = true;
-        symbol = "🔴";
-      };
-      aws = {
-        symbol = " ";
-      };
-      conda = {
-        symbol = " ";
-      };
-      dart = {
-        symbol = " ";
-      };
-      docker_context = {
-        symbol = " ";
-      };
-      elixir = {
-        symbol = " ";
-      };
-      elm = {
-        symbol = " ";
-      };
-      git_branch = {
-        symbol = " ";
-      };
-      golang = {
-        symbol = " ";
-      };
-      hg_branch = {
-        symbol = " ";
-      };
-      java = {
-        symbol = " ";
-      };
-      julia = {
-        symbol = " ";
-      };
-      nim = {
-        symbol = " ";
-      };
-      nix_shell = {
-        symbol = " ";
-      };
-      nodejs = {
-        symbol = " ";
-      };
-      package = {
-        symbol = " ";
-      };
-      perl = {
-        symbol = " ";
-      };
-      php = {
-        symbol = " ";
-      };
-      python = {
-        symbol = " ";
-      };
-      ruby = {
-        symbol = " ";
-      };
-      rust = {
-        symbol = " ";
-      };
-      swift = {
-        symbol = "ﯣ ";
-      };
-    };
-  };
-};
-    home-manager.enable = true;
-    hyprlock = {
-      enable = true;
-      settings = {
-        general = {
-          disable_loading_bar = true;
-          grace = 10;
-          hide_cursor = true;
-          no_fade_in = false;
+        # FIRST LINE/ROW: Info & Status
+        username = {
+          format = " [╭─$user]($style)@";
+          show_always = true;
+          style_root = "bold red";
+          style_user = "bold red";
         };
-        # background = [
-        #   {
-        #     path = "/home/${username}/Pictures/Wallpapers/Wall.png";
-        #     blur_passes = 3;
-        #     blur_size = 8;
-        #   }
-        # ];
-        # image = [
-        #   {
-        #     path = "/home/${username}/.config/5-cm.jpg";
-        #     size = 150;
-        #     border_size = 4;
-        #     border_color = "rgb(0C96F9)";
-        #     rounding = -1; # Negative means circle
-        #     position = "0, 200";
-        #     halign = "center";
-        #     valign = "center";
-        #   }
-        # ];
-        # input-field = [
-        #   {
-        #     size = "200, 50";
-        #     position = "0, -80";
-        #     monitor = "";
-        #     dots_center = true;
-        #     fade_on_empty = false;
-        #     font_color = "rgb(CFE6F4)";
-        #     inner_color = "rgb(657DC2)";
-        #     outer_color = "rgb(0D0E15)";
-        #     outline_thickness = 5;
-        #     placeholder_text = "Password...";
-        #     shadow_passes = 2;
-        #   }
-        # ];
-      };
-    };
+        hostname = {
+          disabled = false;
+          format = "[$hostname]($style) in ";
+          ssh_only = false;
+          style = "bold dimmed red";
+          trim_at = "-";
+        };
+        directory = {
+          style = "purple";
+          truncate_to_repo = true;
+          truncation_length = 0;
+          truncation_symbol = "repo: ";
+        };
+        sudo = { disabled = false; };
+        git_status = {
+          ahead = "⇡${count}";
+          behind = "⇣${count}";
+          deleted = "x";
+          diverged = "⇕⇡${ahead_count}⇣${behind_count}";
+          style = "white";
+        };
+        cmd_duration = {
+          disabled = false;
+          format = "took [$duration]($style)";
+          min_time = 1;
+        };
 
+        # SECOND LINE/ROW: Prompt
+        battery = {
+          charging_symbol = "";
+          disabled = true;
+          discharging_symbol = "";
+          full_symbol = "";
+          display = [
+            {
+              disabled = false;
+              style = "bold red";
+              threshold = 15;
+            }
+            {
+              disabled = true;
+              style = "bold yellow";
+              threshold = 50;
+            }
+            {
+              disabled = true;
+              style = "bold green";
+              threshold = 80;
+            }
+          ];
+        };
+        time = {
+          disabled = true;
+          format = " 🕙 $time($style)\n";
+          style = "bright-white";
+          time_format = "%T";
+        };
+        character = {
+          error_symbol = " [×](bold red)";
+          success_symbol = " [╰─λ](bold red)";
+        };
+
+        # SYMBOLS
+        status = {
+          disabled = false;
+          format =
+            "[[$symbol$status_common_meaning$status_signal_name$status_maybe_int]]($style)";
+          map_symbol = true;
+          pipestatus = true;
+          symbol = "🔴";
+        };
+        aws = { symbol = " "; };
+        conda = { symbol = " "; };
+        dart = { symbol = " "; };
+        docker_context = { symbol = " "; };
+        elixir = { symbol = " "; };
+        elm = { symbol = " "; };
+        git_branch = { symbol = " "; };
+        golang = { symbol = " "; };
+        hg_branch = { symbol = " "; };
+        java = { symbol = " "; };
+        julia = { symbol = " "; };
+        nim = { symbol = " "; };
+        nix_shell = { symbol = " "; };
+        nodejs = { symbol = " "; };
+        package = { symbol = " "; };
+        perl = { symbol = " "; };
+        php = { symbol = " "; };
+        python = { symbol = " "; };
+        ruby = { symbol = " "; };
+        rust = { symbol = " "; };
+        swift = { symbol = "ﯣ "; };
+      };
+      home-manager.enable = true;
+      hyprlock = {
+        enable = true;
+        settings = {
+          general = {
+            disable_loading_bar = true;
+            grace = 10;
+            hide_cursor = true;
+            no_fade_in = false;
+          };
+          # background = [
+          #   {
+          #     path = "/home/${username}/Pictures/Wallpapers/Wall.png";
+          #     blur_passes = 3;
+          #     blur_size = 8;
+          #   }
+          # ];
+          # image = [
+          #   {
+          #     path = "/home/${username}/.config/5-cm.jpg";
+          #     size = 150;
+          #     border_size = 4;
+          #     border_color = "rgb(0C96F9)";
+          #     rounding = -1; # Negative means circle
+          #     position = "0, 200";
+          #     halign = "center";
+          #     valign = "center";
+          #   }
+          # ];
+          # input-field = [
+          #   {
+          #     size = "200, 50";
+          #     position = "0, -80";
+          #     monitor = "";
+          #     dots_center = true;
+          #     fade_on_empty = false;
+          #     font_color = "rgb(CFE6F4)";
+          #     inner_color = "rgb(657DC2)";
+          #     outer_color = "rgb(0D0E15)";
+          #     outline_thickness = 5;
+          #     placeholder_text = "Password...";
+          #     shadow_passes = 2;
+          #   }
+          # ];
+        };
+      };
+
+    };
   };
 }
