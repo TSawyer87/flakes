1830a4c3d48a386e6d589891a6f4857a35a7d6d0
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-02 09:50:45 -0500

    home.pointerCursor

--- hosts/magic/config.nix
+++ hosts/magic/config.nix
@@ -315,7 +315,7 @@ in {
     #cursor.package = pkgs.rose-pine-cursor;
     #cursor.name = "Rose-Pine-Cursor";
     # cursor.name = "Bibata-Modern-Ice";
-    cursor.size = 24;
+    # cursor.size = 24;
     fonts = {
       monospace = {
         # Change this:
