2f466199cd39fb0f84b03804d31650a07222c8f1
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-02 14:29:43 -0500

    xdg-mime

--- hosts/magic/home.nix
+++ hosts/magic/home.nix
@@ -111,9 +111,9 @@ in {
   xdg = {
     configFile."mimeapps.list".text = ''
       [Default Applications]
-      x-scheme-handler/http=zen.desktop
-      x-scheme-handler/https=zen.desktop
-      text/html=zen.desktop
+      # x-scheme-handler/http=zen.desktop
+      # x-scheme-handler/https=zen.desktop
+      # text/html=zen.desktop
       application/pdf=org.pwmt.zathura.desktop
     '';
     userDirs = {
