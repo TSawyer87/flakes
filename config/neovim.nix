c914613fdb47bddd72b983346b7c088d47a2ead8
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-03 08:21:47 -0500

    neovim-unwrapped

--- config/neovim.nix
+++ config/neovim.nix
@@ -10,7 +10,7 @@ in {
     neovim = {
       enable = true;
       package =
-        pkgs.neovim-unwrapped.override; # This should pick up from the nightly overlay
+        pkgs.neovim-unwrapped; # This should pick up from the nightly overlay
       defaultEditor = true;
       viAlias = true;
       vimAlias = true;
