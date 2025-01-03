937e6bb38c353689658fdd9d7745f3d6e7488120
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-03 08:19:09 -0500

    neovim-nightly

--- hosts/magic/config.nix
+++ hosts/magic/config.nix
@@ -549,7 +549,11 @@ in {
     settings = {
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
-      substituters = [ "https://hyprland.cachix.org" "https://wezterm.cachix.org" "https://ghostty.cachix.org" ];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://wezterm.cachix.org"
+        "https://ghostty.cachix.org"
+      ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
