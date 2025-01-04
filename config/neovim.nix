f2250ac10fe4ff8d5606f2ab656aa6e60132002e
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-04 08:06:02 -0500

    neovim

--- config/neovim.nix
+++ config/neovim.nix
@@ -8,7 +8,7 @@ let
 in {
   programs = {
     neovim = {
-      enable = false;
+      enable = true;
       package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
       defaultEditor = true;
       viAlias = true;
