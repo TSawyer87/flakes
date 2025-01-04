a4a9116aca14142510beeb1b9e419f8dd91c69df
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-04 07:44:03 -0500

    nightly nixvim

--- config/nixvim.nix
+++ config/nixvim.nix
@@ -3,6 +3,7 @@
 {
   programs.nixvim = {
     enable = true;
+    package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
 
     colorschemes.catppuccin.enable = true;
     plugins.lualine.enable = true;
