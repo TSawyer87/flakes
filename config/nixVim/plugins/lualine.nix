166e5f33c5ba3bce717da8b7acc11db7a8f6d6bb
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-04 12:46:59 -0500

    lualine

--- config/nixVim/plugins/lualine.nix
+++ config/nixVim/plugins/lualine.nix
@@ -55,20 +55,20 @@
               __unkeyed-1 = "filename";
               path = 1;
             }
-            {
-              __unkeyed-1.__raw = ''
-                function()
-                  local icon = " "
-                  local status = require("copilot.api").status.data
-                  return icon .. (status.message or " ")
-                end,
-
-                cond = function()
-                 local ok, clients = pcall(vim.lsp.get_clients, { name = "copilot", bufnr = 0 })
-                 return ok and #clients > 0
-                end,
-              '';
-            }
+            # {
+            #   __unkeyed-1.__raw = ''
+            #     function()
+            #       local icon = " "
+            #       local status = require("copilot.api").status.data
+            #       return icon .. (status.message or " ")
+            #     end,
+            #
+            #     cond = function()
+            #      local ok, clients = pcall(vim.lsp.get_clients, { name = "copilot", bufnr = 0 })
+            #      return ok and #clients > 0
+            #     end,
+            #   '';
+            # }
           ];
           lualine_y = [{ __unkeyed-1 = "progress"; }];
           lualine_z = [{ __unkeyed-1 = "location"; }];
