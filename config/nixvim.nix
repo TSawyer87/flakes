35f0d71a6d27a1e93bc6d9c1f53f998f6380a0c5
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-04 07:40:56 -0500

    nixvim

--- config/nixvim.nix
+++ config/nixvim.nix
@@ -3,20 +3,8 @@
 {
   programs.nixvim = {
     enable = true;
-    # Here you configure your Neovim settings
-    options = {
-      number = true; # Show line numbers
-      relativenumber = true; # Show relative line numbers
-      colorcolumn = "80";
-    };
-    plugins = {
-      # Enable plugins
-      lualine.enable = true;
-    };
+
     colorschemes.catppuccin.enable = true;
-    extraConfigLua = ''
-      -- Add custom Lua here for additional configurations
-      vim.wo.signcolumn = "yes"
-    '';
+    plugins.lualine.enable = true;
   };
 }
