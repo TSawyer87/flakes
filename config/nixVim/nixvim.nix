b09db74649803ecf09b8e7ed6f67b891913ba216
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-04 16:41:48 -0500

    modular options

--- config/nixVim/nixvim.nix
+++ config/nixVim/nixvim.nix
@@ -16,6 +16,7 @@
     ./plugins/bufferline.nix
     ./plugins/lualine.nix
     ./plugins/oil.nix
+    ./options.nix
   ];
   programs.nixvim = {
     enable = true;
@@ -51,74 +52,6 @@
     # NOTE: You can change these options as you wish!
     #  For more options, you can see `:help option-list`
     # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=globals#opts
-    opts = {
-      # Show line numbers
-      number = true;
-      # You can also add relative line numbers, to help with jumping.
-      #  Experiment for yourself to see if you like it!
-      #relativenumber = true
-
-      # Enable mouse mode, can be useful for resizing splits for example!
-      mouse = "a";
-
-      # Don't show the mode, since it's already in the statusline
-      showmode = false;
-
-      #  See `:help 'clipboard'`
-      clipboard = {
-        providers = {
-          wl-copy.enable = true; # For Wayland
-          xsel.enable = true; # For X11
-        };
-
-        # Sync clipboard between OS and Neovim
-        #  Remove this option if you want your OS clipboard to remain independent.
-        register = "unnamedplus";
-      };
-
-      # Enable break indent
-      breakindent = true;
-
-      # Save undo history
-      undofile = true;
-
-      # Case-insensitive searching UNLESS \C or one or more capital letters in search term
-      ignorecase = true;
-      smartcase = true;
-
-      # Keep signcolumn on by default
-      signcolumn = "yes";
-
-      # Decrease update time
-      updatetime = 250;
-
-      # Decrease mapped sequence wait time
-      # Displays which-key popup sooner
-      timeoutlen = 300;
-
-      # Configure how new splits should be opened
-      splitright = true;
-      splitbelow = true;
-
-      # Sets how neovim will display certain whitespace characters in the editor
-      #  See `:help 'list'`
-      #  See `:help 'listchars'`
-      list = true;
-      # NOTE: .__raw here means that this field is raw lua code
-      listchars.__raw = "{ tab = '» ', trail = '·', nbsp = '␣' }";
-
-      # Preview subsitutions live, as you type!
-      inccommand = "split";
-
-      # Show which line your cursor is on
-      cursorline = true;
-
-      # Minimal number of screen lines to keep above and below the cursor
-      scrolloff = 10;
-
-      # See `:help hlsearch`
-      hlsearch = true;
-    };
 
     # [[ Basic Keymaps ]]
     #  See `:help vim.keymap.set()`
@@ -140,32 +73,29 @@
         mode = "t";
         key = "<Esc><Esc>";
         action = "<C-\\><C-n>";
-        options = {
-          desc = "Exit terminal mode";
-        };
+        options = { desc = "Exit terminal mode"; };
       }
       # TIP: Disable arrow keys in normal mode
-      /*
-      {
-        mode = "n";
-        key = "<left>";
-        action = "<cmd>echo 'Use h to move!!'<CR>";
-      }
-      {
-        mode = "n";
-        key = "<right>";
-        action = "<cmd>echo 'Use l to move!!'<CR>";
-      }
-      {
-        mode = "n";
-        key = "<up>";
-        action = "<cmd>echo 'Use k to move!!'<CR>";
-      }
-      {
-        mode = "n";
-        key = "<down>";
-        action = "<cmd>echo 'Use j to move!!'<CR>";
-      }
+      /* {
+           mode = "n";
+           key = "<left>";
+           action = "<cmd>echo 'Use h to move!!'<CR>";
+         }
+         {
+           mode = "n";
+           key = "<right>";
+           action = "<cmd>echo 'Use l to move!!'<CR>";
+         }
+         {
+           mode = "n";
+           key = "<up>";
+           action = "<cmd>echo 'Use k to move!!'<CR>";
+         }
+         {
+           mode = "n";
+           key = "<down>";
+           action = "<cmd>echo 'Use j to move!!'<CR>";
+         }
       */
       # Keybinds to make split navigation easier.
       #  Use CTRL+<hjkl> to switch between windows
@@ -175,42 +105,30 @@
         mode = "n";
         key = "<C-h>";
         action = "<C-w><C-h>";
-        options = {
-          desc = "Move focus to the left window";
-        };
+        options = { desc = "Move focus to the left window"; };
       }
       {
         mode = "n";
         key = "<C-l>";
         action = "<C-w><C-l>";
-        options = {
-          desc = "Move focus to the right window";
-        };
+        options = { desc = "Move focus to the right window"; };
       }
       {
         mode = "n";
         key = "<C-j>";
         action = "<C-w><C-j>";
-        options = {
-          desc = "Move focus to the lower window";
-        };
+        options = { desc = "Move focus to the lower window"; };
       }
       {
         mode = "n";
         key = "<C-k>";
         action = "<C-w><C-k>";
-        options = {
-          desc = "Move focus to the upper window";
-        };
+        options = { desc = "Move focus to the upper window"; };
       }
     ];
 
     # https://nix-community.github.io/nixvim/NeovimOptions/autoGroups/index.html
-    autoGroups = {
-      kickstart-highlight-yank = {
-        clear = true;
-      };
-    };
+    autoGroups = { kickstart-highlight-yank = { clear = true; }; };
 
     # [[ Basic Autocommands ]]
     #  See `:help lua-guide-autocommands`
@@ -220,7 +138,7 @@
       #  Try it with `yap` in normal mode
       #  See `:help vim.highlight.on_yank()`
       {
-        event = ["TextYankPost"];
+        event = [ "TextYankPost" ];
         desc = "Highlight when yanking (copying) text";
         group = "kickstart-highlight-yank";
         callback.__raw = ''
@@ -237,9 +155,9 @@
 
       # Detect tabstop and shiftwidth automatically
       # https://nix-community.github.io/nixvim/plugins/sleuth/index.html
-      sleuth = {
-        enable = true;
-      };
+      sleuth = { enable = true; };
+
+      lz-n = { enable = true; }; # lazy loading
 
       # Highlight todo, notes, etc in comments
       # https://nix-community.github.io/nixvim/plugins/todo-comments/index.html
@@ -249,13 +167,43 @@
           signs = true;
         };
       };
+      grug-far = {
+        enable = true;
+        lazyLoad = { settings = { cmd = "GrugFar"; }; };
+      };
+      glow = {
+        enable = true;
+        lazyLoad.settings.ft = "markdown";
+      };
+      obsidian = {
+        enable = true;
+        settings = {
+          completion = {
+            min_chars = 2;
+            nvim_cmp = true;
+          };
+          new_notes_location = "current_dir";
+          workspaces = [
+            {
+              name = "notes";
+              path = "~/notes";
+            }
+            {
+              name = "2nd_brain";
+              path = "~/notes/2nd_brain";
+            }
+          ];
+        };
+      };
+
     };
 
     # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraplugins#extraplugins
-    extraPlugins = with pkgs.vimPlugins; [
-      # Useful for getting pretty icons, but requires a Nerd Font.
-      nvim-web-devicons # TODO: Figure out how to configure using this with telescope
-    ];
+    extraPlugins = with pkgs.vimPlugins;
+      [
+        # Useful for getting pretty icons, but requires a Nerd Font.
+        nvim-web-devicons # TODO: Figure out how to configure using this with telescope
+      ];
 
     # TODO: Figure out where to move this
     # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraplugins#extraconfigluapre
