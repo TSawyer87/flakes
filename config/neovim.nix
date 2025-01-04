4ca06abbb63487e370c1cde685341fe14ac784e3
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-04 08:07:14 -0500

    remove nvim

--- config/neovim.nix
+++ config/neovim.nix
@@ -8,7 +8,7 @@ let
 in {
   programs = {
     neovim = {
-      enable = true;
+      enable = false;
       package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
       defaultEditor = true;
       viAlias = true;
