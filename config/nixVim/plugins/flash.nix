774765d9cb796e5d1e60d7ce4dea6d00d28904e7
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-05 09:26:16 -0500

    flash

--- config/nixVim/plugins/flash.nix
+++ config/nixVim/plugins/flash.nix
@@ -1,26 +1,23 @@
-{
-  programs.nixvim = {
-    # Neo-tree is a Neovim plugin to browse the file system
-    # https://nix-community.github.io/nixvim/plugins/neo-tree/index.html?highlight=neo-tree#pluginsneo-treepackage
-    plugins.flash = {
-      enable = true;
-      keymaps = [
-
-        {
-          mode = [ "n" "x" "o" ];
-          key = "s";
-          action = ''
-            function()
-                             require('' flash ").jump(), end,";
-          options = { desc = "Open parent directory"; };
-        }
-        {
-          mode = "c";
-          key = "<c-s>";
-          action = "require(" flash ").toggle()";
-          options = { desc = "Toggle Flash Search"; };
-        }
-      ];
-    };
+programs.nixvim = {
+  plugins.flash = {
+    enable = true;
+    keymaps = [
+      {
+        mode = [ "n" "x" "o" ];
+        key = "s";
+        action = ''
+          function()
+            require("flash").jump()
+          end
+        '';
+        options = { desc = "Flash Jump"; };
+      }
+      {
+        mode = "c";
+        key = "<c-s>";
+        action = "require('flash').toggle()";
+        options = { desc = "Toggle Flash Search"; };
+      }
+    ];
   };
 }
