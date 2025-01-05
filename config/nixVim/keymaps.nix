24e427fe2e62c0797f43178fb4ad9da9984ab3bb
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-05 09:29:42 -0500

    flash

--- config/nixVim/keymaps.nix
+++ config/nixVim/keymaps.nix
@@ -49,6 +49,23 @@
         action = "<CMD>Oil<CR>";
         options = { desc = "Open parent directory"; };
       }
+      {
+        mode = [ "n" "x" "o" ];
+        key = "s";
+        action = ''
+          function()
+            require("flash").jump()
+          end
+        '';
+        options = { desc = "Flash Jump"; };
+      }
+      {
+        mode = "c";
+        key = "<c-s>";
+        action = "require('flash').toggle()";
+        options = { desc = "Toggle Flash Search"; };
+      }
+
     ];
   };
 }
