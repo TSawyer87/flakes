4b90ab51320143d6fd703f1e50f4401ea3e531b4
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-06 11:37:41 -0500

    markdown-nvim

--- config/nixVim/plugins/markdown.nix
+++ config/nixVim/plugins/markdown.nix
@@ -1,8 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs }: {
   programs.nixvim = {
     plugins = {
-      markdown-nvim.enable = true;
-      markdown-nvim.package = pkgs.vimPlugins.markdown-nvim;
+      markdown-nvim = {
+        enable = true;
+        package = pkgs.vimPlugins.markdown-nvim;
+      };
     };
   };
 }
