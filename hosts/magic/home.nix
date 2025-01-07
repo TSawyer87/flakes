79843f72dec40181ee6c66c07b47f50cc15c6a0b
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-07 12:09:40 -0500

    programs.nh

--- hosts/magic/home.nix
+++ hosts/magic/home.nix
@@ -234,6 +234,12 @@ in {
     };
   };
 
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/jr/flakes";
+  };
   programs = {
     gh.enable = true;
     btop = {
