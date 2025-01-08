3c54c5ae5ffa0df148db0b75d09efbf80f4e0812
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-08 14:31:26 -0500

    path hypr

--- config/hypr/hyprland.nix
+++ config/hypr/hyprland.nix
@@ -1,7 +1,7 @@
 { lib, username, host, config, ... }:
 
 let
-  inherit (import ../hosts/${host}/variables.nix)
+  inherit (import ../../hosts/${host}/variables.nix)
     browser terminal keyboardLayout;
 in with lib; {
   wayland.windowManager.hyprland = {
