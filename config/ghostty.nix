926f33490328f4761f3cc21940c91a0fcc771a86
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-08 10:55:25 -0500

    ghostty modular

--- config/ghostty.nix
+++ config/ghostty.nix
@@ -1,6 +1,16 @@
 { ... }: {
   programs.ghostty = {
     enable = true;
+    keybindings = {
+      "super+c" = "copy_to_clipboard";
+
+      "super+shift+h" = "goto_split:left";
+      "super+shift+j" = "goto_split:bottom";
+      "super+shift+k" = "goto_split:top";
+      "super+shift+l" = "goto_split:right";
+
+      "ctrl+page_up" = "jump_to_prompt:-1";
+    };
     settings = {
       font-size = 14;
       font-family = "Fira-Code-Mono Nerd Font";
@@ -18,16 +28,6 @@
 
       # Disables ligatures
       font-feature = [ "-liga" "-dlig" "-calt" ];
-    };
-    keybindings = {
-      "super+c" = "copy_to_clipboard";
-
-      "super+shift+h" = "goto_split:left";
-      "super+shift+j" = "goto_split:bottom";
-      "super+shift+k" = "goto_split:top";
-      "super+shift+l" = "goto_split:right";
-
-      "ctrl+page_up" = "jump_to_prompt:-1";
     };
   };
 }
