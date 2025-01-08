2ea89ca6cfceced85473910f61f1dc1ff23c255c
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-08 15:21:01 -0500

    zen-mode twilight

--- config/nixVim/plugins/zen-mode.nix
+++ config/nixVim/plugins/zen-mode.nix
@@ -1,15 +1,6 @@
 {
   programs.nixvim = {
     plugins.twilight.enable = true;
-    plugins.zen-mode = {
-      enable = true;
-      luaConfig = ''
-            require("zen-mode").setup({
-        	plugins = {
-        		twilight = { enabled = true },
-        	},
-        })
-      '';
-    };
+    plugins.zen-mode = { enable = true; };
   };
 }
