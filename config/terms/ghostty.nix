0c0f7822b131e53906ae1b59691fd737edfe29be
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-09 06:27:51 -0500

    ghostty

--- config/terms/ghostty.nix
+++ config/terms/ghostty.nix
@@ -1,29 +1,19 @@
 { ... }: {
   programs.ghostty = {
     enable = true;
-    # keybindings = {
-    #   "super+c" = "copy_to_clipboard";
-    #
-    #   "super+shift+h" = "goto_split:left";
-    #   "super+shift+j" = "goto_split:bottom";
-    #   "super+shift+k" = "goto_split:top";
-    #   "super+shift+l" = "goto_split:right";
-    #
-    #   "ctrl+page_up" = "jump_to_prompt:-1";
-    # };
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    installVimSyntax = true;
     settings = {
       font-size = 14;
       font-family = "Fira-Code-Mono Nerd Font";
       window-decoration = false;
       confirm-close-surface = false;
 
-      # The default is a bit intense for my liking
-      # but it looks good with some themes
       unfocused-split-opacity = 0.9;
       background-opacity = 0.8;
       background-blur-radius = 20;
 
-      # Some macOS settings
       window-theme = "dark";
 
       # Disables ligatures
