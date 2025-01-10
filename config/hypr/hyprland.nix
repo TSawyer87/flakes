74bb59d53e65696fd737946c564b753da53b02b5
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-10 11:45:33 -0500

    trying cursor without hyprland env calls

--- config/hypr/hyprland.nix
+++ config/hypr/hyprland.nix
@@ -22,8 +22,6 @@ in with lib; {
       env = QT_AUTO_SCREEN_SCALE_FACTOR, 1
       env = SDL_VIDEODRIVER, x11
       env = MOZ_ENABLE_WAYLAND, 1
-      env = HYPRCURSOR_THEME,rose-pine-hyprcursor
-      env = HYPRCURSOR_SIZE,26
       exec-once = dbus-update-activation-environment --systemd --all
       exec-once = systemctl --user import-environment QT_QPA_PLATFORMTHEME WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
       exec-once = killall -q swww;sleep .5 && swww init
