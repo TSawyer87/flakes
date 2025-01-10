797baf63cb670f7de5d891855220cce1cca9880f
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-10 17:47:21 -0500

    noice

--- config/nixVim/plugins/noice.nix
+++ config/nixVim/plugins/noice.nix
@@ -1,46 +1,49 @@
 {
-  programs.nixvim.plugins.noice.settings = {
+  programs.nixvim.plugins.noice = {
     enable = true;
-    notify = { enabled = true; };
-    messages = {
-      enabled =
-        true; # Adds a padding-bottom to neovim statusline when set to false for some reason
-    };
-    lsp = {
-      message = { enabled = false; };
-      progress = {
-        enabled = false;
-        view = "mini";
+    settings = {
+      enable = true;
+      notify = { enabled = true; };
+      messages = {
+        enabled =
+          true; # Adds a padding-bottom to neovim statusline when set to false for some reason
       };
-    };
-    popupmenu = {
-      enabled = true;
-      backend = "nui";
-    };
-    format = {
-      filter = {
-        pattern = [
-          ":%s*%%s*s:%s*"
-          ":%s*%%s*s!%s*"
-          ":%s*%%s*s/%s*"
-          "%s*s:%s*"
-          ":%s*s!%s*"
-          ":%s*s/%s*"
-        ];
-        icon = "";
-        lang = "regex";
+      lsp = {
+        message = { enabled = false; };
+        progress = {
+          enabled = false;
+          view = "mini";
+        };
       };
-      replace = {
-        pattern = [
-          ":%s*%%s*s:%w*:%s*"
-          ":%s*%%s*s!%w*!%s*"
-          ":%s*%%s*s/%w*/%s*"
-          "%s*s:%w*:%s*"
-          ":%s*s!%w*!%s*"
-          ":%s*s/%w*/%s*"
-        ];
-        icon = "󱞪";
-        lang = "regex";
+      popupmenu = {
+        enabled = true;
+        backend = "nui";
+      };
+      format = {
+        filter = {
+          pattern = [
+            ":%s*%%s*s:%s*"
+            ":%s*%%s*s!%s*"
+            ":%s*%%s*s/%s*"
+            "%s*s:%s*"
+            ":%s*s!%s*"
+            ":%s*s/%s*"
+          ];
+          icon = "";
+          lang = "regex";
+        };
+        replace = {
+          pattern = [
+            ":%s*%%s*s:%w*:%s*"
+            ":%s*%%s*s!%w*!%s*"
+            ":%s*%%s*s/%w*/%s*"
+            "%s*s:%w*:%s*"
+            ":%s*s!%w*!%s*"
+            ":%s*s/%w*/%s*"
+          ];
+          icon = "󱞪";
+          lang = "regex";
+        };
       };
     };
   };
