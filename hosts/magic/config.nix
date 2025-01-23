f58aa1f4b962179afc45b0f3d712a71c9ff1072d
installer <installer@gmail.com>
2025-01-23 12:39:33 -0500

    modular stylix

--- hosts/magic/config.nix
+++ hosts/magic/config.nix
@@ -8,6 +8,7 @@ in {
     ./users.nix
     ./boot.nix
     ./systemPackages.nix
+    ./stylix.nix
     ../../modules/amd-drivers.nix
     ../../modules/nvidia-drivers.nix
     ../../modules/nvidia-prime-drivers.nix
@@ -17,171 +18,60 @@ in {
     #    ../../config/firefox.nix
   ];
 
-  # environment.systemPackages = with pkgs; [
-  #   inputs.nix-inspect.packages.${pkgs.system}.default
-  #   inputs.rose-pine-hyprcursor.packages.${pkgs.system}.default
-  #   #rose-pine-cursor
-  #   vim
-  #   vulkan-loader
-  #   vulkan-validation-layers
-  #   vulkan-tools
-  #   amdvlk
-  #   profile-sync-daemon
-  #   wget
-  #   killall
-  #   eza
-  #   git
-  #   cmatrix
-  #   lolcat
-  #   efibootmgr
-  #   htop
-  #   pokemon-colorscripts-mac
-  #   libvirt
-  #   lxqt.lxqt-policykit
-  #   lm_sensors
-  #   unzip
-  #   unrar
-  #   libnotify
-  #   v4l-utils
-  #   ydotool
-  #   duf
-  #   ncdu
-  #   wl-clipboard
-  #   pciutils
-  #   ffmpeg
-  #   socat
-  #   cowsay
-  #   ripgrep
-  #   lshw
-  #   bat
-  #   pkg-config
-  #   meson
-  #   hyprpicker
-  #   ninja
-  #   brightnessctl
-  #   virt-viewer
-  #   swappy
-  #   appimage-run
-  #   networkmanagerapplet
-  #   markdownlint-cli
-  #   markdownlint-cli2
-  #   yad
-  #   inxi
-  #   playerctl
-  #   nh
-  #   nixfmt-classic
-  #   #discord
-  #   stdenv
-  #   swww
-  #   grim
-  #   slurp
-  #   file-roller
-  #   swaynotificationcenter
-  #   imv
-  #   mpv
-  #   gimp
-  #   pavucontrol
-  #   tree
-  #   cachix
-  #   #spotify
-  #   #neovide
-  #   dconf2nix
-  #   greetd.tuigreet
-  #   hyprls
-  #   jq
-  #   nodePackages.prettier
-  #   prettierd
-  #   ruff
-  #   lazygit
-  #   shfmt
-  #   shellcheck
-  #   nixd
-  #   nodejs_22
-  #   nil
-  #   lua-language-server
-  #   bash-language-server
-  #   stylua
-  #   cliphist
-  #   wofi
-  #   pyprland
-  #   zig_0_12
-  #   unipicker
-  #   nvtopPackages.amd
-  #   dmidecode
-  #   alsa-utils
-  #   nix-diff
-  #   manix
-  #   linuxKernel.packages.linux_zen.cpupower
-  #   tradingview
-  #   dconf-editor
-  #   rose-pine-cursor
-  #   pfetch-rs
-  #   rustup
-  #   rustc
-  #   rustfmt
-  #   cargo
-  #   fwupd
-  #   openssl
-  #   pkg-config
-  #   gccgo14
-  #   go
-  #   gomuks
-  #   olm
-  # ];
   # Styling Options
-  stylix = {
-    enable = true;
-    #image = ../../config/wallpapers/Lofi-Cafe1.png;
-    # image = ../../config/wallpapers/bookmarks.png;
-    # image = ../../config/wallpapers/keinbackup.png;
-    image = ../../config/wallpapers/Under_Starlit_Sky.png;
-    base16Scheme = {
-      base00 = "1D2021";
-      base01 = "32302F";
-      base02 = "504945";
-      base03 = "665C54";
-      base04 = "928374";
-      base05 = "A89984";
-      base06 = "D5C4A1";
-      base07 = "FDF4C1";
-      base08 = "FB543F";
-      base09 = "FE8625";
-      base0A = "FAC03B";
-      base0B = "95C085";
-      base0C = "8BA59B";
-      base0D = "0D6678";
-      base0E = "8F4673";
-      base0F = "A87322";
-    };
-    polarity = "dark";
-    opacity.terminal = 0.8;
-    # cursor.package = pkgs.bibata-cursors;
-    cursor.package =
-      inputs.rose-pine-hyprcursor.packages.${pkgs.system}.default;
-    cursor.name = "BreezeX-RosePine-Linux";
-    # cursor.name = "Bibata-Modern-Ice";
-    cursor.size = 26;
-    fonts = {
-      monospace = {
-        package = pkgs.nerd-fonts.fira-mono;
-        name = "Fira-Mono";
-      };
-      sansSerif = {
-        package = pkgs.montserrat;
-        name = "Montserrat";
-      };
-      serif = {
-        package = pkgs.montserrat;
-        name = "Montserrat";
-      };
-      sizes = {
-        applications = 12;
-        terminal = 15;
-        desktop = 11;
-        popups = 12;
-      };
-    };
-  };
+  # stylix = {
+  #   enable = true;
+  #   #image = ../../config/wallpapers/Lofi-Cafe1.png;
+  #   # image = ../../config/wallpapers/bookmarks.png;
+  #   # image = ../../config/wallpapers/keinbackup.png;
+  #   image = ../../config/wallpapers/Under_Starlit_Sky.png;
+  #   base16Scheme = {
+  #     base00 = "1D2021";
+  #     base01 = "32302F";
+  #     base02 = "504945";
+  #     base03 = "665C54";
+  #     base04 = "928374";
+  #     base05 = "A89984";
+  #     base06 = "D5C4A1";
+  #     base07 = "FDF4C1";
+  #     base08 = "FB543F";
+  #     base09 = "FE8625";
+  #     base0A = "FAC03B";
+  #     base0B = "95C085";
+  #     base0C = "8BA59B";
+  #     base0D = "0D6678";
+  #     base0E = "8F4673";
+  #     base0F = "A87322";
+  #   };
+  #   polarity = "dark";
+  #   opacity.terminal = 0.8;
+  #   # cursor.package = pkgs.bibata-cursors;
+  #   cursor.package =
+  #     inputs.rose-pine-hyprcursor.packages.${pkgs.system}.default;
+  #   cursor.name = "BreezeX-RosePine-Linux";
+  #   # cursor.name = "Bibata-Modern-Ice";
+  #   cursor.size = 26;
+  #   fonts = {
+  #     monospace = {
+  #       package = pkgs.nerd-fonts.fira-mono;
+  #       name = "Fira-Mono";
+  #     };
+  #     sansSerif = {
+  #       package = pkgs.montserrat;
+  #       name = "Montserrat";
+  #     };
+  #     serif = {
+  #       package = pkgs.montserrat;
+  #       name = "Montserrat";
+  #     };
+  #     sizes = {
+  #       applications = 12;
+  #       terminal = 15;
+  #       desktop = 11;
+  #       popups = 12;
+  #     };
+  #   };
+  # };
 
   # Extra Module Options
   # drivers.amdgpu.enable = true;
