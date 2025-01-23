1b68ce68ca0d655d98dd83eb2428f489b5ba227b
installer <installer@gmail.com>
2025-01-23 12:07:49 -0500

    waybar

--- config/hypr/hyprland.nix
+++ config/hypr/hyprland.nix
@@ -31,6 +31,7 @@ in with lib; {
       exec-once = wl-paste --type text --watch cliphist store # clipboard store text data
       exec-once = wl-paste --type image --watch cliphist store # clipboard store image data
       exec-once = sleep 1.5 && swww img /home/${username}/Pictures/Wallpapers/nord-space.png
+      exec-once = waybar &
       monitor=,preferred,auto,1
       monitor=DP-1, 3840x2160, 0x0, 1.5
       general {
