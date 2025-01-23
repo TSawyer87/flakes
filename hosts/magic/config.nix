863b61d7ec964387ae8dba6059eaff679ca41999
installer <installer@gmail.com>
2025-01-23 12:32:49 -0500

    wayland cachix

--- hosts/magic/config.nix
+++ hosts/magic/config.nix
@@ -470,17 +470,19 @@ in {
       experimental-features = [ "nix-command" "flakes" ];
       substituters = [
         "https://hyprland.cachix.org"
-        "https://wezterm.cachix.org"
         "https://ghostty.cachix.org"
         "https://neovim-nightly.cachix.org"
         "https://yazi.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://nixpkgs-wayland.cachix.org"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
         "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
         "neovim-nightly.cachix.org-1:feIoInHRevVEplgdZvQDjhp11kYASYCE2NGY9hNrwxY="
         "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
     # gc = {
