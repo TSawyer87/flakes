5e436cb75ceebd69b6ea92c135648c789d28a8d1
installer <installer@gmail.com>
2025-01-24 18:46:48 -0500

    footclient

---
+++ config/terms/foot.nix
@@ -0,0 +1,17 @@
+{ ... }: {
+  programs = {
+    foot = {
+      enable = true;
+      server.enable = true;
+      settings = {
+        main = {
+          term = "xterm-256color";
+          font = "Fira-Mono:size=15";
+          dpi-aware = "no";
+        };
+        mouse = { hide-when-typing = "yes"; };
+      };
+    };
+
+  };
+}
--- modules/homeManagerModules/homePrograms.nix
+++ modules/homeManagerModules/homePrograms.nix
@@ -63,18 +63,18 @@ in {
         };
       };
     };
-    foot = {
-      enable = true;
-      server.enable = true;
-      settings = {
-        main = {
-          term = "xterm-256color";
-          font = "JetBrainsMono Nerd Font Mono:size=15";
-          dpi-aware = "no";
-        };
-        mouse = { hide-when-typing = "yes"; };
-      };
-    };
+    # foot = {
+    #   enable = true;
+    #   server.enable = true;
+    #   settings = {
+    #     main = {
+    #       term = "xterm-256color";
+    #       font = "JetBrainsMono Nerd Font Mono:size=15";
+    #       dpi-aware = "no";
+    #     };
+    #     mouse = { hide-when-typing = "yes"; };
+    #   };
+    # };
 
     zathura = { enable = true; };
 
