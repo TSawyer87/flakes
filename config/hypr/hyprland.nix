bba3babe5c442f2cef4815bb617bb34bad45032a
installer <installer@gmail.com>
2025-01-24 15:05:55 -0500

    hyprland

--- config/hypr/hyprland.nix
+++ config/hypr/hyprland.nix
@@ -6,6 +6,7 @@ let
 in with lib; {
   home.packages = with pkgs; [
     inputs.rose-pine-hyprcursor.packages.${pkgs.system}.default
+    inputs.hyprland.packages.${pkgs.system}.hyprland-qtutils
     wayland
     wl-clipboard
     cliphist
