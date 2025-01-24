25f74a9ee0b9023c12b7f6ec6fbbe72998c89d72
installer <installer@gmail.com>
2025-01-24 14:52:28 -0500

    xdg default *

--- config/hypr/hyprland.nix
+++ config/hypr/hyprland.nix
@@ -28,11 +28,11 @@ in with lib; {
   xdg = {
     portal = {
       enable = true;
-      config.common.default = "*";
+      config.common.default = [ "hyprland" "gtk" ];
       extraPortals = [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal ];
       configPackages = [
-        pkgs.xdg-desktop-portal-gtk
         pkgs.xdg-desktop-portal-hyprland
+        pkgs.xdg-desktop-portal-gtk
         pkgs.xdg-desktop-portal
       ];
     };
