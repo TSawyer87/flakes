24c617d026a666d110e63e9401e7a5f46e90a571
installer <installer@gmail.com>
2025-01-24 09:38:51 -0500

    homeFiles

--- hosts/magic/home.nix
+++ hosts/magic/home.nix
@@ -30,36 +30,37 @@ in {
     ../../modules/homeManagerModules/homePrograms.nix
     ../../modules/homeManagerModules/homePackages.nix
     ../../modules/homeManagerModules/homeServices.nix
+    ../../modules/homeManagerModules/homeFiles.nix
   ];
 
   # Place Files Inside Home Directory
-  home.file."Pictures/Wallpapers" = {
-    source = ../../config/wallpapers;
-    recursive = true;
-  };
-  home.file.".config/wlogout/icons" = {
-    source = ../../config/wlogout;
-    recursive = true;
-  };
-  home.file.".config/hypr/pyprland.toml" = {
-    source = ../../config/hypr/pyprland.toml;
-    recursive = true;
-  };
-
-  # home.file.".face.icon".source = ../../config/face.jpg;
-  # home.file.".config/face.jpg".source = ../../config/face.jpg;
-  home.file.".config/swappy/config".text = ''
-    [Default]
-    save_dir=/home/${username}/Pictures/Screenshots
-    save_filename_format=swappy-%Y%m%d-%H%M%S.png
-    show_panel=false
-    line_size=5
-    text_size=20
-    text_font=Ubuntu
-    paint_mode=brush
-    early_exit=true
-    fill_shape=false
-  '';
+  # home.file."Pictures/Wallpapers" = {
+  #   source = ../../config/wallpapers;
+  #   recursive = true;
+  # };
+  # home.file.".config/wlogout/icons" = {
+  #   source = ../../config/wlogout;
+  #   recursive = true;
+  # };
+  # home.file.".config/hypr/pyprland.toml" = {
+  #   source = ../../config/hypr/pyprland.toml;
+  #   recursive = true;
+  # };
+  #
+  # # home.file.".face.icon".source = ../../config/face.jpg;
+  # # home.file.".config/face.jpg".source = ../../config/face.jpg;
+  # home.file.".config/swappy/config".text = ''
+  #   [Default]
+  #   save_dir=/home/${username}/Pictures/Screenshots
+  #   save_filename_format=swappy-%Y%m%d-%H%M%S.png
+  #   show_panel=false
+  #   line_size=5
+  #   text_size=20
+  #   text_font=Ubuntu
+  #   paint_mode=brush
+  #   early_exit=true
+  #   fill_shape=false
+  # '';
 
   # Create XDG Dirs
   xdg = {
---
+++ modules/homeManagerModules/homeFiles.nix
@@ -0,0 +1,31 @@
+{ username, ... }: {
+  # Place Files Inside Home Directory
+  home.file."Pictures/Wallpapers" = {
+    source = ../../config/wallpapers;
+    recursive = true;
+  };
+  home.file.".config/wlogout/icons" = {
+    source = ../../config/wlogout;
+    recursive = true;
+  };
+  home.file.".config/hypr/pyprland.toml" = {
+    source = ../../config/hypr/pyprland.toml;
+    recursive = true;
+  };
+
+  # home.file.".face.icon".source = ../../config/face.jpg;
+  # home.file.".config/face.jpg".source = ../../config/face.jpg;
+  home.file.".config/swappy/config".text = ''
+    [Default]
+    save_dir=/home/${username}/Pictures/Screenshots
+    save_filename_format=swappy-%Y%m%d-%H%M%S.png
+    show_panel=false
+    line_size=5
+    text_size=20
+    text_font=Ubuntu
+    paint_mode=brush
+    early_exit=true
+    fill_shape=false
+  '';
+
+}
