eee24321f7c21a2b6cbca0cd631b4e71b25f7bdc
installer <installer@gmail.com>
2025-01-24 19:06:47 -0500

    footclient

--- config/terms/foot.nix
+++ config/terms/foot.nix
@@ -6,18 +6,13 @@
     enable = true;
     settings = {
       main = {
+        term = "xterm-256color";
         font = "JetBrains Mono:size=15";
         # Adjust spacing and line-height if needed
         spacing = 0;
         line-height = "1.0";
       };
 
-      fonts = {
-        regular = {
-          family = "JetBrains Mono";
-          style = "Regular";
-        };
-      };
     };
   };
 }
