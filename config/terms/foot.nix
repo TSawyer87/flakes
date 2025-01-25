9bf748a56128bc7826b83f875c402b4a1c217cf6
installer <installer@gmail.com>
2025-01-24 19:08:04 -0500

    footserver

--- config/terms/foot.nix
+++ config/terms/foot.nix
@@ -8,9 +8,6 @@
       main = {
         term = "xterm-256color";
         font = "JetBrains Mono:size=15";
-        # Adjust spacing and line-height if needed
-        spacing = 0;
-        line-height = "1.0";
       };
 
     };
