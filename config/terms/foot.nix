7c104763eee58f760ebff0be8228823a6bbbc21a
installer <installer@gmail.com>
2025-01-24 19:18:40 -0500

    foot.ini

--- config/terms/foot.nix
+++ config/terms/foot.nix
@@ -5,12 +5,7 @@
   programs.foot = {
     enable = true;
     server = { enable = true; };
-    settings = {
-      main = {
-        term = "xterm-256color";
-        font = "JetBrains Mono:size=15";
-      };
 
-    };
   };
+  xdg.configFile."foot/foot.ini".source = ./foot.ini;
 }
