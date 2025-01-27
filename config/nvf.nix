ff7fc5c8ba73092763fb134bb02443e9615347f2
installer <installer@gmail.com>
2025-01-27 12:58:42 -0500

    alias nvf

--- config/nvf.nix
+++ config/nvf.nix
@@ -6,7 +6,7 @@
     settings = {
       vim = {
       package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
-      viAlias = true;
+      viAlias = false;
       vimAlias = true;
       lsp = { enable = true; };
       languages = {
