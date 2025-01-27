f7f3e10e3392ddd5f22d8415a77a6e3e1c4e2f04
installer <installer@gmail.com>
2025-01-27 10:08:35 -0500

    aerial nvf

--- config/nvf.nix
+++ config/nvf.nix
@@ -4,10 +4,31 @@
     # your settings need to go into the settings attribute set
     # most settings are documented in the appendix
     settings = {
-  vim.package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+      vim.package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
       vim.viAlias = true;
       vim.vimAlias = false;
       vim.lsp = { enable = true; };
+    };
+    config = {
+      vim.lazy.plugins = {
+      "aerial.nvim" = {
+      package = pkgs.vimPlugins.aerial-nvim;
+      setupModule = "aerial";
+
+      # Explicitly mark plugin as lazy. You don't need this if you define one of
+      # the trigger "events" below
+      lazy = true;
+
+      # load on keymap
+      keys = [
+        {
+          key = "<leader>a";
+          action = ":AerialToggle<CR>";
+        }
+      ];
+    };
+  };
+      };
     };
   };
 }
