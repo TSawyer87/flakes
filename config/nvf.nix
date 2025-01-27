8d1185c4a4b7678867cb128bfdc0f6e99262df2a
installer <installer@gmail.com>
2025-01-27 10:21:18 -0500

    toggleterm

--- config/nvf.nix
+++ config/nvf.nix
@@ -4,11 +4,26 @@
     # your settings need to go into the settings attribute set
     # most settings are documented in the appendix
     settings = {
-      vim.package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
-      vim.viAlias = true;
-      vim.vimAlias = false;
-      vim.lsp = { enable = true; };
-      vim.languages.nix.enable = true;
+      vim = {
+      package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+      viAlias = true;
+      vimAlias = false;
+      lsp = { enable = true; };
+      languages = {
+      nix.enable = true;
     };
+    lazy.plugins = {
+      {
+    toggleterm-nvim = {
+      package = "toggleterm-nvim";
+      setupModule = "toggleterm";
+      setupOpts = cfg.setupOpts;
+  
+      after = "require('toggleterm').do_something()";
+      cmd = ["ToggleTerm"];
+    };
+    };
+    };
+  };
   };
   }
