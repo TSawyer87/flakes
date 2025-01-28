57f449e0f806b81c91ed8e4dfaee0354b26f052a
installer <installer@gmail.com>
2025-01-28 11:37:18 -0500

    nvf

--- config/nvf.nix
+++ config/nvf.nix
@@ -10,9 +10,27 @@
         theme.enable = false;
         viAlias = false;
         vimAlias = true;
-        lsp = { enable = true; };
+        spellcheck = {
+          enable = true;
+        }
+        lsp = {
+            formatOnSave = true;
+            lspkind.enable = true;
+            trouble.enable = true;
+            lsplines.enable = true;
+      };
+
+        debugger = {
+          nvim-dab = {
+            enable = true;
+            ui.enable = true;
+          };
+        };
+
         languages = {
           enableLSP = true;
+          enableFormat = true;
+          enableExtraDiagnostics = true;
           enableTreesitter = true;
           nix.enable = true;
           nix.lsp.enable = true;
@@ -21,59 +39,47 @@
           nix.treesitter.enable = true;
           markdown.enable = true;
           bash.enable = true;
+          rust = {
+            enable = true;
+            crates.enable = true;
+          };
         };
 
         leaderKey = " ";
         lineNumberMode = "relative";
 
-        luaConfigRC = {
-          globalsScript = ''
-            --vim.g.mapleader = " " -- Set leader to space
-          '';
-          optionsScript = ''
-            vim.opt.wrap = false
-            vim.opt.clipboard = "unnamedplus" -- External clipboard support
-            vim.opt.number = true
-            vim.opt.laststatus = 0 -- Never show status line
-            --vim.opt.relativenumber = true
-            vim.opt.mouse = ""
+        tabline = { nvimBufferline.enable = true; };
 
-            -- Tab config
-            vim.opt.tabstop = 4 -- A TAB character looks like 2 spaces
-            vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-            vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-            vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
-          '';
-
-          basic = "";
-
-          /* theme = ''
-
-             '';
-          */
-
-          extraPlugins = with pkgs.vimPlugins; {
-
-            fwatch = { package = fwatch-nvim; };
-            tabline = { nvimBufferline.enable = true; };
-
-            theme.name = "tokyonight";
-            autopairs.nvim-autopairs.enable = true;
-            statusline.lualine.enable = true;
-            autocomplete = {
-              nvim-cmp.mappings.confirm = "<C-;>";
-              nvim-cmp.enable = true;
-            };
-            binds = { whichKey.enable = true; };
-            lazy.plugins = {
-              toggleterm-nvim = {
-                package = "toggleterm-nvim";
-                setupModule = "toggleterm";
-                cmd = [ "ToggleTerm" ];
-              };
-            };
+        theme = {
+          name = "tokyonight";
+          style = "storm";
+          transparent = true;
+        };
+        autopairs.nvim-autopairs.enable = true;
+        statusline = {
+          lualine = {
+            enable = true;
+            theme = "tokyonight";
           };
         };
+        autocomplete = {
+          nvim-cmp.mappings.confirm = "<C-;>";
+          nvim-cmp.enable = true;
+        };
+        binds = { whichKey.enable = true; };
+        telescope.enable = true;
+
+        utility = {
+          surround.enable = true;
+          diffview-nvim.enable = true;
+          motion = {
+          hop.enable = true;
+          leap.enable = true;
+          precognition.enable = true;
+        };
+        };
+
+
       };
     };
   };
