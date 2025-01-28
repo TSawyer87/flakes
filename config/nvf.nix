c0b20d967aa4caaf228b1334cf2ce27fc2f6ac9d
installer <installer@gmail.com>
2025-01-28 11:18:32 -0500

    nvf

--- config/nvf.nix
+++ config/nvf.nix
@@ -7,6 +7,7 @@
     settings = {
       vim = {
         package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+        theme.enable = false;
         viAlias = false;
         vimAlias = true;
         lsp = { enable = true; };
@@ -25,21 +26,52 @@
         leaderKey = " ";
         lineNumberMode = "relative";
 
-        tabline = { nvimBufferline.enable = true; };
+        luaConfigRC = {
+          globalsScript = ''
+            --vim.g.mapleader = " " -- Set leader to space
+          '';
+          optionsScript = ''
+            vim.opt.wrap = false
+            vim.opt.clipboard = "unnamedplus" -- External clipboard support
+            vim.opt.number = true
+            vim.opt.laststatus = 0 -- Never show status line
+            --vim.opt.relativenumber = true
+            vim.opt.mouse = ""
 
-        theme.name = "tokyonight";
-        autopairs.nvim-autopairs.enable = true;
-        statusline.lualine.enable = true;
-        autocomplete = {
-          nvim-cmp.mappings.confirm = "<C-;>";
-          nvim-cmp.enable = true;
-        };
-        binds = { whichKey.enable = true; };
-        lazy.plugins = {
-          toggleterm-nvim = {
-            package = "toggleterm-nvim";
-            setupModule = "toggleterm";
-            cmd = [ "ToggleTerm" ];
+            -- Tab config
+            vim.opt.tabstop = 4 -- A TAB character looks like 2 spaces
+            vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+            vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+            vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+          '';
+
+          basic = "";
+
+          /* theme = ''
+
+             '';
+          */
+
+          extraPlugins = with pkgs.vimPlugins; {
+
+            fwatch = { package = fwatch-nvim; };
+            tabline = { nvimBufferline.enable = true; };
+
+            theme.name = "tokyonight";
+            autopairs.nvim-autopairs.enable = true;
+            statusline.lualine.enable = true;
+            autocomplete = {
+              nvim-cmp.mappings.confirm = "<C-;>";
+              nvim-cmp.enable = true;
+            };
+            binds = { whichKey.enable = true; };
+            lazy.plugins = {
+              toggleterm-nvim = {
+                package = "toggleterm-nvim";
+                setupModule = "toggleterm";
+                cmd = [ "ToggleTerm" ];
+              };
+            };
           };
         };
       };
