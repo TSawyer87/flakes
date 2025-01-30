206c44b38da5ae802f16e63e9aa59b7389ed1d2e
TSawyer87 <sawyerjr.25@gmail.com>
2025-01-30 13:50:31 -0500

    fix codecompanion syntax

--- config/nixVim/plugins/autopairs.nix
+++ config/nixVim/plugins/autopairs.nix
@@ -2,9 +2,7 @@
   # Inserts matching pairs of parens, brackets, etc.
   # https://nix-community.github.io/nixvim/plugins/nvim-autopairs/index.html
   programs.nixvim = {
-    plugins.nvim-autopairs = {
-      enable = true;
-    };
+    plugins.nvim-autopairs = { enable = true; };
 
     # If you want to automatically add `(` after selecting a function or method
     # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraconfiglua#extraconfiglua
