1629994a3e01e01cf0a0fab7367a485fc6f91113
TSawyer87 <sawyerjr.25@gmail.com>
2025-02-07 09:03:16 -0500

    stylix colorscheme

--- modules/nixosModules/stylix.nix
+++ modules/nixosModules/stylix.nix
@@ -38,6 +38,24 @@ with lib;
       #   base0E = "8F4673";
       #   base0F = "A87322";
       # };
+      base16Scheme = { # Gruvbox Material Dark, Hard
+        base00 = "202020";
+        base01 = "2a2827";
+        base02 = "504945";
+        base03 = "5a524c";
+        base04 = "bdae93";
+        base05 = "ddc7a1";
+        base06 = "ebdbb2";
+        base07 = "fbf1c7";
+        base08 = "ea6962";
+        base09 = "e78a4e";
+        base0A = "d8a657";
+        base0B = "a9b665";
+        base0C = "89b482";
+        base0D = "7daea3";
+        base0E = "d3869b";
+        base0F = "bd6f3e";
+      };
 
       # base16Scheme = { # TokyoNightStorm
       #   base00 = "24283B";
@@ -76,24 +94,24 @@ with lib;
       #   base0E = "a485dd";
       #   base0F = "773440";
       # };
-      base16Scheme = {  # Catppuccin Mocha
-        base00 = "1e1e2e"; # base
-        base01 = "181825"; # mantle
-        base02 = "313244"; # surface0
-        base03 = "45475a"; # surface1
-        base04 = "585b70"; # surface2
-        base05 = "cdd6f4"; # text
-        base06 = "f5e0dc"; # rosewater
-        base07 = "b4befe"; # lavender
-        base08 = "f38ba8"; # red
-        base09 = "fab387"; # peach
-        base0A = "f9e2af"; # yellow
-        base0B = "a6e3a1"; # green
-        base0C = "94e2d5"; # teal
-        base0D = "89b4fa"; # blue
-        base0E = "cba6f7"; # mauve
-        base0F = "f2cdcd"; # flamingo
-      };
+      # base16Scheme = {  # Catppuccin Mocha
+      #   base00 = "1e1e2e"; # base
+      #   base01 = "181825"; # mantle
+      #   base02 = "313244"; # surface0
+      #   base03 = "45475a"; # surface1
+      #   base04 = "585b70"; # surface2
+      #   base05 = "cdd6f4"; # text
+      #   base06 = "f5e0dc"; # rosewater
+      #   base07 = "b4befe"; # lavender
+      #   base08 = "f38ba8"; # red
+      #   base09 = "fab387"; # peach
+      #   base0A = "f9e2af"; # yellow
+      #   base0B = "a6e3a1"; # green
+      #   base0C = "94e2d5"; # teal
+      #   base0D = "89b4fa"; # blue
+      #   base0E = "cba6f7"; # mauve
+      #   base0F = "f2cdcd"; # flamingo
+      # };
       polarity = "dark";
       opacity.terminal = 0.8;
       # cursor.package = pkgs.bibata-cursors;
