0446f6d05fbf798be02c84f42a7dcacfff06ad1b
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-01 16:43:18 -0500

    wpaperd

--- config/sway/sway.nix
+++ config/sway/sway.nix
@@ -17,31 +17,27 @@ in {
     config = rec {
       modifier = mod;
       terminal = "${pkgs.ghostty}/bin/ghostty";
-      startup = [{ command = "firefox"; }]; # Moved here
-      # Keybindings will be merged from keybinds.nix
+      startup = [{ command = "firefox"; }];
       floating.border = 0;
       window.border = 0;
     };
     extraConfig = ''
       seat * xcursor_theme bibata_modern_ice 26
-      # Explicitly set modifier
       set $mod Mod4
 
-      # Start other utilities
       exec waybar &
       exec nm-applet --indicator
       exec wl-paste --type text --watch cliphist store
       exec wl-paste --type image --watch cliphist store
 
       output DP-1 {
-      bg ~/Pictures/Wallpapers/mountains1.jpg fill
+        bg ~/Pictures/Wallpapers/mountains1.jpg fill
         mode 3840x2160@65Hz
         scale 1.5
         pos 0 0
       }
 
       output HDMI-A-1 {
-      # background ~/Pictures/Wallpapers/mountains2.jpg fill
         mode 1920x1080@100Hz
         scale 1
         pos 2560 0  
@@ -51,7 +47,7 @@ in {
         repeat_delay 300
         repeat_rate 50
       }
-      # exec sleep 2 && ${pkgs.wpaperd}/bin/wpaperd -d
+      exec sleep 2 && ${pkgs.wpaperd}/bin/wpaperd -d
     '';
   };
 
@@ -68,6 +64,7 @@ in {
       RestartSec = 5;
     };
   };
+
   services = {
     network-manager-applet.enable = true;
     cliphist.enable = true;
@@ -93,3 +90,4 @@ in {
     wtype
   ];
 }
+
