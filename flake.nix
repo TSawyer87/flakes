41a80fd3db973fa70cf56f877dfd4f98b894737e
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-01 17:33:06 -0500

    swayfx

--- flake.nix
+++ flake.nix
@@ -13,15 +13,15 @@
     nix-inspect.url = "github:bluskript/nix-inspect";
     rose-pine-hyprcursor.url = "github:ndom91/rose-pine-hyprcursor";
     stylix.url = "github:danth/stylix";
-     scenefx = {
-+      url = "github:wlrfx/scenefx";
-+      inputs.nixpkgs.follows = "nixpkgs";
-+    };
-     swayfx = {
-       url = "github:WillPower3309/swayfx";
-+      inputs.nixpkgs.follows = "nixpkgs";
-+      inputs.scenefx.follows = "scenefx";
-     };
+    scenefx = {
+      url = "github:wlrfx/scenefx";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    swayfx = {
+      url = "github:WillPower3309/swayfx";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.scenefx.follows = "scenefx";
+    };
     # rust-overlay = {
     #   url = "github:oxalica/rust-overlay";
     #   inputs.nixpkgs.follows = "nixpkgs";
@@ -32,82 +32,76 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    nix-index-database,
-    swayfx,
-    scenefx,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
+  outputs = { self, nixpkgs, home-manager, nix-index-database, swayfx, scenefx
+    , ... }@inputs:
+    let
+      inherit (self) outputs;
 
-    systems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
+      systems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-    host = "magic";
-    username = "jr";
-    pkgsForSystem = system:
-      import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    packages =
-      forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-
-    # Or 'nixpkgs-fmt'
-    formatter =
-      forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
-
-    # Your custom packages and modifications, exported as overlays
-    overlays = import ./overlays {inherit inputs;};
-
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#hostname'
-    nixosConfigurations = {
-      "${host}" = nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit systems;
-          inherit inputs;
-          inherit username;
-          inherit host;
-          inherit outputs;
+      host = "magic";
+      username = "jr";
+      pkgsForSystem = system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
         };
-        modules = [
-          ./hosts/${host}/config.nix
-          inputs.stylix.nixosModules.stylix
-          home-manager.nixosModules.home-manager
-          nix-index-database.nixosModules.nix-index
-          # ({ pkgs, ... }: {
-          #   nixpkgs.overlays = [ rust-overlay.overlays.default ];
-          #   environment.systemPackages =
-          #     [ pkgs.rust-bin.stable.latest.default ];
-          # })
-          {
-            # Apply the overlays to the NixOS system
-            # nixpkgs.overlays = overlays;
-            home-manager.extraSpecialArgs = {
-              inherit username;
-              inherit inputs;
-              inherit host;
-              inherit systems;
-            };
-            # home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "backup";
-            home-manager.users.${username} = import ./hosts/${host}/home.nix;
-            nixpkgs.config.allowUnfree = true;
-          }
-        ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      packages =
+        forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+
+      # Or 'nixpkgs-fmt'
+      formatter =
+        forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+      # Your custom packages and modifications, exported as overlays
+      overlays = import ./overlays { inherit inputs; };
+
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#hostname'
+      nixosConfigurations = {
+        "${host}" = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit systems;
+            inherit inputs;
+            inherit username;
+            inherit host;
+            inherit outputs;
+          };
+          modules = [
+            ./hosts/${host}/config.nix
+            inputs.stylix.nixosModules.stylix
+            home-manager.nixosModules.home-manager
+            nix-index-database.nixosModules.nix-index
+            # ({ pkgs, ... }: {
+            #   nixpkgs.overlays = [ rust-overlay.overlays.default ];
+            #   environment.systemPackages =
+            #     [ pkgs.rust-bin.stable.latest.default ];
+            # })
+            {
+              # Apply the overlays to the NixOS system
+              # nixpkgs.overlays = overlays;
+              home-manager.extraSpecialArgs = {
+                inherit username;
+                inherit inputs;
+                inherit host;
+                inherit systems;
+              };
+              # home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
+              home-manager.users.${username} = import ./hosts/${host}/home.nix;
+              nixpkgs.config.allowUnfree = true;
+            }
+          ];
+        };
       };
     };
-  };
 }
