8c421e780221a3cec681b058f187cffe0c52232b
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-01 14:47:56 -0500

    wpaperd

--- modules/nixosModules/xdg.nix
+++ modules/nixosModules/xdg.nix
@@ -1,12 +1,12 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   # Extra Portal Configuration
   xdg.portal = {
     enable = true;
     wlr.enable = true;
-    extraPortals = [pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal];
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal ];
     configPackages = [
       pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-hyprland
+      # pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal
     ];
   };
