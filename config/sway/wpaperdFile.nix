1328b7a98d018142c77e82ae85aca61e214ef9c7
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-01 14:19:49 -0500

    fontconfig

--- config/sway/wpaperdFile.nix
+++ config/sway/wpaperdFile.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   home.file.".config/wpaperd/config.toml".text = ''
         [default]
     path = "/home/jr/Pictures/Wallpapers/"
@@ -6,5 +6,7 @@
     transition-time = 600
 
   '';
+  home.file.".cache/fontconfig".source = pkgs.writeTextDir "empty" "";
+  home.file.".cache/fontconfig".recursive = true;
 
 }
