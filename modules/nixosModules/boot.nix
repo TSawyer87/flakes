5cbc3e6a939bc1d7829c4f08d2efdd346c482eb5
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-02 09:42:30 -0500

    modularizing home programs

--- modules/nixosModules/boot.nix
+++ modules/nixosModules/boot.nix
@@ -1,28 +1,24 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{ pkgs, config, ... }: {
   boot = {
     kernelPackages = pkgs.linuxPackages_zen;
-    kernelModules = ["v4l2loopback"];
-    extraModulePackages = [config.boot.kernelPackages.v4l2loopback];
-    kernel.sysctl = {"vm.max_map_count" = 2147483642;};
+    kernelModules = [ "v4l2loopback" ];
+    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    kernel.sysctl = { "vm.max_map_count" = 2147483642; };
     loader = {
       efi.canTouchEfiVariables = true;
       systemd-boot = {
         enable = true;
         configurationLimit = 20;
-      }
-    }
+      };
+    };
     # Appimage Support
     binfmt.registrations.appimage = {
       wrapInterpreterInShell = false;
       interpreter = "${pkgs.appimage-run}/bin/appimage-run";
       recognitionType = "magic";
       offset = 0;
-      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-      magicOrExtension = ''\x7fELF....AI\x02'';
+      mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
+      magicOrExtension = "\\x7fELF....AI\\x02";
     };
     plymouth.enable = true;
   };
