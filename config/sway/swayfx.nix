0b1f4118c9218b2c7a1113f6249a1c926912d432
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-02 08:52:30 -0500

    swayfx config

--- config/sway/swayfx.nix
+++ config/sway/swayfx.nix
@@ -1,34 +1,32 @@
 { ... }: {
   # SceneFX configuration
-  wayland.windowManager.sway = {
 
-    extraConfig = ''
-      ### Appearance
-      # window corner radius in px
-      corner_radius 6
+  home.file.".config/sway/config.d/swayfx" = ''
+    ### Appearance
+    # window corner radius in px
+    corner_radius 6
 
-      # Window background blur
-      blur enable
-      blur_xray off
-      blur_passes 2
-      blur_radius 2
+    # Window background blur
+    blur enable
+    blur_xray off
+    blur_passes 2
+    blur_radius 2
 
-      # Shadows
-      shadows enable
-      shadows_on_csd off
-      shadow_blur_radius 8
-      shadow_color #0000007F
+    # Shadows
+    shadows enable
+    shadows_on_csd off
+    shadow_blur_radius 8
+    shadow_color #0000007F
 
-      # Waybar blur
-      layer_effects "waybar" blur enable; shadows enable; corner_radius 10
+    # Waybar blur
+    layer_effects "waybar" blur enable; shadows enable; corner_radius 10
 
-      # Inactive window dimming
-      default_dim_inactive 0.2
-      dim_inactive_colors.unfocused #000000FF
-      dim_inactive_colors.urgent #900000FF
+    # Inactive window dimming
+    default_dim_inactive 0.2
+    dim_inactive_colors.unfocused #000000FF
+    dim_inactive_colors.urgent #900000FF
 
-      # Move minimized windows into Scratchpad (enable|disable)
-      # scratchpad_minimize disable
-    '';
-  };
+    # Move minimized windows into Scratchpad (enable|disable)
+    # scratchpad_minimize disable
+  '';
 }
