8367c6313d8919f17bc9fb721ea92ea396c30cb7
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-03 10:11:57 -0500

    rustaceanvim

--- config/nvim/plugins/rustaceanvim.lua
+++ config/nvim/plugins/rustaceanvim.lua
@@ -62,22 +62,17 @@ vim.g.rustaceanvim = {
 
 -- Additional configuration for rustaceanvim
 local opts = vim.g.rustaceanvim or {}
-if require("lazyvim").has("mason.nvim") then
-  local package_path = require("mason-registry").get_package("codelldb"):get_install_path()
-  local codelldb = package_path .. "/extension/adapter/codelldb"
-  local library_path = package_path .. "/extension/lldb/lib/liblldb.dylib"
-  local uname = io.popen("uname"):read("*l")
-  if uname == "Linux" then
-    library_path = package_path .. "/extension/lldb/lib/liblldb.so"
-  end
-  opts.dap = {
-    adapter = require("rustaceanvim.config").get_codelldb_adapter(codelldb, library_path),
-  }
+local uname = io.popen("uname"):read("*l")
+local package_path = os.getenv("HOME") .. "/.local/share/nvim/mason/packages/codelldb"
+local codelldb = package_path .. "/extension/adapter/codelldb"
+local library_path = package_path .. "/extension/lldb/lib/liblldb.dylib"
+if uname == "Linux" then
+  library_path = package_path .. "/extension/lldb/lib/liblldb.so"
 end
+opts.dap = {
+  adapter = require("rustaceanvim.config").get_codelldb_adapter(codelldb, library_path),
+}
 vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
 if vim.fn.executable("rust-analyzer") == 0 then
-  require("lazyvim").error(
-    "**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/",
-    { title = "rustaceanvim" }
-  )
+  print("**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/")
 end
