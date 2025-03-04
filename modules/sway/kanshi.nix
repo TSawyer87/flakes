70aed99bfa57a91c1e84866897db731257fa201b
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-04 09:17:52 -0500

    kanshi

--- modules/sway/kanshi.nix
+++ modules/sway/kanshi.nix
@@ -18,7 +18,7 @@ in
     };
     services.kanshi = {
       enable = true;
-      systemdTarget = "sway-session.target";
+      #      systemdTarget = "sway-session.target";
       settings = [
         {
           profile.name = "home";
