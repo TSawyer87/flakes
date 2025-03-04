66b9614a469d41031fadf98b1648becfe539632f
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-04 09:23:10 -0500

    kanshi

--- modules/sway/kanshi.nix
+++ modules/sway/kanshi.nix
@@ -24,14 +24,15 @@ in
           profile.name = "home";
           profile.outputs = [
             {
-              criteria = "DP-1";
-              mode = "3840x2160@65";
+              criteria = "Sceptre Tech Inc Sceptre Z27 (DP-1)";
+              # mode = "3840x2160@65";
+              mode = "2560x1440@65";
               status = "enable";
               position = "0,0";
               scale = 1.5;
             }
             {
-              criteria = "HDMI-A-1";
+              criteria = "ViewSonic Corporation VA2447 100Hz WE3234703733 (HDMI-A-1)";
               mode = "1920x1080@100";
               position = "2560,0";
               scale = 1.0;
