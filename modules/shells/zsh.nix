62a3383da8e3f7800b9e2cb08acbf9ffe980c69f
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-05 17:52:48 -0500

    nixCats zsh alias

--- modules/shells/zsh.nix
+++ modules/shells/zsh.nix
@@ -1,11 +1,4 @@
-{
-  pkgs,
-  host,
-  username,
-  inputs,
-  ...
-}:
-{
+{ pkgs, host, username, inputs, ... }: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -14,12 +7,7 @@
     oh-my-zsh = {
       package = pkgs.oh-my-zsh;
       enable = true;
-      plugins = [
-        "git"
-        "sudo"
-        "rust"
-        "fzf"
-      ];
+      plugins = [ "git" "sudo" "rust" "fzf" ];
     };
     profileExtra = ''
       #if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
@@ -112,11 +100,14 @@
     shellAliases = {
       sv = "sudo nvim";
       fr = "nh os switch --hostname ${host} /home/${username}/flakes";
-      ft = "nh os test --hostname ${host} /home/${username}/flakes"; # dont save generation to boot menu
+      ft =
+        "nh os test --hostname ${host} /home/${username}/flakes"; # dont save generation to boot menu
       fu = "nh os switch --hostname ${host} --update /home/${username}/flakes";
-      upd = "sudo nixos-rebuild switch --upgrade --flake /home/${username}/flakes";
+      upd =
+        "sudo nixos-rebuild switch --upgrade --flake /home/${username}/flakes";
       rebuild = "/home/jr/scripts/performance_hook.sh";
-      ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+      ncg =
+        "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       opts = "man home-configuration.nix";
       zd = "zeditor";
       lg = "lazygit";
@@ -132,6 +123,7 @@
       v = "nvim";
       vz = "NVIM_APPNAME='lazy' nvim";
       vk = "NVIM_APPNAME='kick' nvim";
+      vc = "nix run /home/jr/flakes/modules/nixCats";
       cat = "bat --style snip --style changes --style header";
       l = "eza -lh --icons=auto"; # long list
       ls = "eza --icons=auto --group-directories-first --icons"; # short list
@@ -139,8 +131,10 @@
       la = "eza -lah --icons --grid --group-directories-first --icons";
       ld = "eza -lhD --icons=auto";
       lt = "eza --icons=auto --tree"; # list folder as tree
-      rbs = "echo starting performance mode && sudo cpupower frequency-set -g performance && nh os switch --hostname ${host} --update /home/${username}/flakes"; # Amd pstate governor
-      powersave = "sudo cpupower frequency-set -g powersave"; # Amd pstate governor
+      rbs =
+        "echo starting performance mode && sudo cpupower frequency-set -g performance && nh os switch --hostname ${host} --update /home/${username}/flakes"; # Amd pstate governor
+      powersave =
+        "sudo cpupower frequency-set -g powersave"; # Amd pstate governor
       # Get the error messages from journalctl
       jctl = "journalctl -p 3 -xb";
       mkdir = "mkdir -p";
