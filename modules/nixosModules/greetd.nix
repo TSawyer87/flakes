23943687bc5b86209bfd9dd0f45a73fc09d18639
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-06 12:43:16 -0500

    regreet

--- modules/nixosModules/greetd.nix
+++ modules/nixosModules/greetd.nix
@@ -1,14 +1,16 @@
 { pkgs, ... }: {
+  programs.regreet.enable = true;
   services.greetd = {
     enable = true;
     settings = {
       default_session.command = ''
-        ${pkgs.greetd.gtkgreet}/bin/gtkgreet \
+        ${pkgs.greetd.regreet}/bin/regreet \
           --time \
           --asterisks \
           --user-menu \
           # --cmd ${pkgs.sway}/bin/sway
           --cmd ${pkgs.hyprland}/bin/Hyprland
+          # "${lib.getExe config.programs.hyprland.package} --config "
            # --cmd uwsm start hyprland.desktop
            # -- cmd {inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/Hyprland
       '';
