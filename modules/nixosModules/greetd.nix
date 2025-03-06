7a3ae4062f92428ed60b11188ce584d572661029
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-06 13:32:24 -0500

    regreet

--- modules/nixosModules/greetd.nix
+++ modules/nixosModules/greetd.nix
@@ -1,25 +1,20 @@
 { pkgs, config, ... }:
 let
-  hypr-conf = pkgs.writeText "greet-config" ''
+  hypr = pkgs.writeText "hypr-greet-config" ''
     exec "${config.programs.regreet.package}/bin/regreet; ${config.programs.hyprland.package}/bin/Hyprland exit"
   '';
 in {
-  services.greetd.settings.default_session = {
-    command =
-      "${config.programs.hyprland.package}/bin/Hyprland --config ${hypr-conf}";
-  };
+
   programs.regreet.enable = true;
-  # services.greetd = {
-  #   enable = true;
-  #   settings = {
-  #     command = "hyprland --config /home/jr/flakes/modules/hypr/hyprland.conf";
-  #     #   default_session.command = ''
-  #     #     ${pkgs.greetd.regreet}/bin/regreet \
-  #     #       --cmd ${pkgs.hyprland}/bin/Hyprland
-  #     #       # --cmd ${pkgs.sway}/bin/sway
-  #     #   '';
-  #   };
-  # };
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command =
+          "${config.programs.hyprland.package}/bin/Hyprland --config ${hypr}";
+      };
+    };
+  };
 
 }
 
