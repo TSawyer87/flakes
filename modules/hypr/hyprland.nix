30b57c42a3dfd64e7161dbcf05932909fe6fd49e
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-06 12:29:04 -0500

    stable hyprland

--- modules/hypr/hyprland.nix
+++ modules/hypr/hyprland.nix
@@ -13,10 +13,11 @@
     [ "xdg-desktop-autostart.target" ];
   wayland.windowManager.hyprland = {
     enable = true;
-    package =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    package = pkgs.stable.hyprland;
+    # package =
+    #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # portalPackage =
+    #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     xwayland = {
       enable = true;
       # hidpi = true;
