04da7f089aade8eeea62a9028837323b86548cce
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-06 10:29:18 -0500

    hypr

--- modules/homeManagerModules/homeFiles.nix
+++ modules/homeManagerModules/homeFiles.nix
@@ -1,17 +1,17 @@
 { username, ... }: {
   # Place Files Inside Home Directory
-  home.file."Pictures/Wallpapers" = {
-    source = ../wallpapers;
-    recursive = true;
-  };
+  # home.file."Pictures/Wallpapers" = {
+  #   source = ../wallpapers;
+  #   recursive = true;
+  # };
   home.file.".config/wlogout/icons" = {
     source = ../wlogout;
     recursive = true;
   };
-  home.file.".config/hypr/pyprland.toml" = {
-    source = ../hypr/pyprland.toml;
-    recursive = true;
-  };
+  # home.file.".config/hypr/pyprland.toml" = {
+  #   source = ../hypr/pyprland.toml;
+  #   recursive = true;
+  # };
   home.file.".jj/config.toml".text = ''
     [ui]
     diff-editor = ["nvim", "-c", "DiffEditor $left $right $output"]
@@ -19,18 +19,18 @@
 
   # home.file.".face.icon".source = ../../config/face.jpg;
   # home.file.".config/face.jpg".source = ../../config/face.jpg;
-  home.file.".config/swappy/config".text = ''
-    [Default]
-    save_dir=/home/${username}/Pictures/Screenshots
-    save_filename_format=swappy-%Y%m%d-%H%M%S.png
-    show_panel=false
-    line_size=5
-    text_size=20
-    text_font=Ubuntu
-    paint_mode=brush
-    early_exit=true
-    fill_shape=false
-  '';
+  # home.file.".config/swappy/config".text = ''
+  #   [Default]
+  #   save_dir=/home/${username}/Pictures/Screenshots
+  #   save_filename_format=swappy-%Y%m%d-%H%M%S.png
+  #   show_panel=false
+  #   line_size=5
+  #   text_size=20
+  #   text_font=Ubuntu
+  #   paint_mode=brush
+  #   early_exit=true
+  #   fill_shape=false
+  # '';
 
   # home.file.".config/wpaperd/config.toml".text = ''
   #       [default]
