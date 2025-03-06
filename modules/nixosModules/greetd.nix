96dda26ce5baa730fc96e561021c3468d6883944
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-06 13:43:16 -0500

    hypr

--- modules/nixosModules/greetd.nix
+++ modules/nixosModules/greetd.nix
@@ -1,18 +1,10 @@
-{ pkgs, config, inputs, lib, ... }:
-let
-  hypr = lib.getExe
-    inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland ''
-      exec "${config.programs.regreet.package}/bin/regreet; ${config.programs.hyprland.package}/bin/Hyprland exit"
-    '';
-in {
-
-  programs.regreet.enable = true;
+{ config, ... }: {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
         command =
-          "${config.programs.hyprland.package}/bin/Hyprland --config ${hypr}";
+          "${config.programs.hyprland.package}/bin/Hyprland --config /home/jr/flakes/modules/hypr/config.nix";
       };
     };
   };
