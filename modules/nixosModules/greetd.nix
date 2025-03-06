b22a79ed7d0be58a68506283521c20800cc4d0fb
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-06 13:49:25 -0500

    hypr

--- modules/nixosModules/greetd.nix
+++ modules/nixosModules/greetd.nix
@@ -1,11 +1,12 @@
-{ config, ... }: {
+{ pkgs, config, ... }: {
   services.greetd = {
     enable = true;
-    settings = {
-      default_session = {
-        command =
-          "${config.programs.hyprland.package}/bin/Hyprland --config /home/jr/flakes/modules/hypr/config.nix";
+    settings = rec {
+      initial_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland";
+        user = "jr";
       };
+      default_session = initial_session;
     };
   };
 
