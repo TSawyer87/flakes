26a8a8b5ca4981ca830eecf630c73732f3c9c1a2
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-06 12:44:15 -0500

    regreet

--- modules/nixosModules/greetd.nix
+++ modules/nixosModules/greetd.nix
@@ -8,11 +8,8 @@
           --time \
           --asterisks \
           --user-menu \
-          # --cmd ${pkgs.sway}/bin/sway
           --cmd ${pkgs.hyprland}/bin/Hyprland
-          # "${lib.getExe config.programs.hyprland.package} --config "
-           # --cmd uwsm start hyprland.desktop
-           # -- cmd {inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/Hyprland
+          # --cmd ${pkgs.sway}/bin/sway
       '';
     };
   };
