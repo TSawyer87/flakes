5a8ce0e86726c1f051fe7840a83a8a21cf37fa97
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-07 10:34:17 -0500

    starship

--- modules/nixosModules/programs.nix
+++ modules/nixosModules/programs.nix
@@ -1,34 +1,34 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   programs = {
     thunderbird.enable = true;
     firefox.enable = true;
-    starship = {
-      enable = true;
-      settings = {
-        add_newline = false;
-        buf = {symbol = " ";};
-        c = {symbol = " ";};
-        directory = {read_only = " 󰌾";};
-        docker_context = {symbol = " ";};
-        fossil_branch = {symbol = " ";};
-        git_branch = {symbol = " ";};
-        golang = {symbol = " ";};
-        hg_branch = {symbol = " ";};
-        hostname = {ssh_symbol = " ";};
-        lua = {symbol = " ";};
-        memory_usage = {symbol = "󰍛 ";};
-        meson = {symbol = "󰔷 ";};
-        nim = {symbol = "󰆥 ";};
-        nix_shell = {symbol = " ";};
-        nodejs = {symbol = " ";};
-        ocaml = {symbol = " ";};
-        package = {symbol = "󰏗 ";};
-        python = {symbol = " ";};
-        rust = {symbol = " ";};
-        swift = {symbol = " ";};
-        zig = {symbol = " ";};
-      };
-    };
+    # starship = {
+    #   enable = true;
+    #   settings = {
+    #     add_newline = false;
+    #     buf = {symbol = " ";};
+    #     c = {symbol = " ";};
+    #     directory = {read_only = " 󰌾";};
+    #     docker_context = {symbol = " ";};
+    #     fossil_branch = {symbol = " ";};
+    #     git_branch = {symbol = " ";};
+    #     golang = {symbol = " ";};
+    #     hg_branch = {symbol = " ";};
+    #     hostname = {ssh_symbol = " ";};
+    #     lua = {symbol = " ";};
+    #     memory_usage = {symbol = "󰍛 ";};
+    #     meson = {symbol = "󰔷 ";};
+    #     nim = {symbol = "󰆥 ";};
+    #     nix_shell = {symbol = " ";};
+    #     nodejs = {symbol = " ";};
+    #     ocaml = {symbol = " ";};
+    #     package = {symbol = "󰏗 ";};
+    #     python = {symbol = " ";};
+    #     rust = {symbol = " ";};
+    #     swift = {symbol = " ";};
+    #     zig = {symbol = " ";};
+    #   };
+    # };
     # dconf.enable = true;
     seahorse.enable = true;
     fuse.userAllowOther = true;
@@ -46,7 +46,7 @@
     };
     thunar = {
       enable = true;
-      plugins = with pkgs.xfce; [thunar-archive-plugin thunar-volman];
+      plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
     };
   };
 }
