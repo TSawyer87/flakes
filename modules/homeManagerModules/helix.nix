c5ac6ff33c1a10c9316fe9162de704e4737daac5
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-27 14:04:21 -0400

    helix.nix config

--- modules/homeManagerModules/helix.nix
+++ modules/homeManagerModules/helix.nix
@@ -2,35 +2,35 @@
   programs.helix = with pkgs; {
     enable = true;
     #defaultEditor = true;
-    extraPackages = [
-      bash-language-server
-      biome
-      clang-tools
-      docker-compose-language-service
-      dockerfile-language-server-nodejs
-      # golangci-lint
-      # golangci-lint-langserver
-      # gopls
-      # gotools
-      helix-gpt
-      # marksman
-      # nil
-      # nixd
-      nixpkgs-fmt
-      # nodePackages.prettier
-      nodePackages.typescript-language-server
-      sql-formatter
-      # ruff
-      # (python3.withPackages
-      #   (p: (with p; [ python-lsp-ruff python-lsp-server ])))
-      # rust-analyzer
-      # taplo
-      taplo-lsp
-      terraform-ls
-      typescript
-      vscode-langservers-extracted
-      yaml-language-server
-    ];
+    # extraPackages = [
+    #   bash-language-server
+    #   biome
+    #   clang-tools
+    #   docker-compose-language-service
+    #   dockerfile-language-server-nodejs
+    #   # golangci-lint
+    #   # golangci-lint-langserver
+    #   # gopls
+    #   # gotools
+    #   helix-gpt
+    #   # marksman
+    #   # nil
+    #   # nixd
+    #   nixpkgs-fmt
+    #   # nodePackages.prettier
+    #   nodePackages.typescript-language-server
+    #   sql-formatter
+    #   # ruff
+    #   # (python3.withPackages
+    #   #   (p: (with p; [ python-lsp-ruff python-lsp-server ])))
+    #   # rust-analyzer
+    #   # taplo
+    #   taplo-lsp
+    #   terraform-ls
+    #   typescript
+    #   vscode-langservers-extracted
+    #   yaml-language-server
+    # ];
 
     settings = {
       # theme = "gruvbox_community";
