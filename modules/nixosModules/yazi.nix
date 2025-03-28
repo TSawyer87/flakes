5ee740ca914ca098f1ffb2159be8973e979031b3
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-28 07:14:47 -0400

    yazi systemPackage

--- modules/nixosModules/yazi.nix
+++ modules/nixosModules/yazi.nix
@@ -1,15 +1,15 @@
 { ... }: {
   programs.yazi = {
     enable = true;
-    settings = {
-      manager = {
-        show_hidden = false;
-        sort_dir_first = true;
-        sort_by = "mtime";
-        sort_reverse = true;
-        linemode = "size";
-        editor = "hx";
-      };
-    };
+    # settings = {
+    #   manager = {
+    #     show_hidden = false;
+    #     sort_dir_first = true;
+    #     sort_by = "mtime";
+    #     sort_reverse = true;
+    #     linemode = "size";
+    #     editor = "hx";
+    #   };
+    # };
   };
 }
