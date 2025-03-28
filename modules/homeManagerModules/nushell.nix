d19a7942066894beee5a06d03231b09136d61f89
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-28 13:23:18 -0400

    nushell aliases

--- modules/homeManagerModules/nushell.nix
+++ modules/homeManagerModules/nushell.nix
@@ -48,7 +48,7 @@
         ft = "nh os test --hostname magic /home/jr/flakes";
         zd = "zeditor";
         ncg =
-          "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+          "nix-collect-garbage --delete-old ; sudo nix-collect-garbage -d ; sudo /run/current-system/bin/switch-to-configuration boot";
 
       };
     };
