6c49ce78566e8ece5d22b4158532e490c7e7e1e3
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-28 06:52:13 -0400

    helix yazi

--- modules/homeManagerModules/helix/default.nix
+++ modules/homeManagerModules/helix/default.nix
@@ -90,6 +90,13 @@
             ":redraw"
             ":reload-all"
           ];
+          C-y = [
+            ":sh rm -f /tmp/unique-file"
+            ":insert-output yazi %{buffer_name} --chooser-file=/tmp/unique-file"
+            ":insert-output echo '\x1b[?1049h\x1b[?2004h' > /dev/tty"
+            ":open %sh{cat /tmp/unique-file}"
+            ":redraw"
+          ];
           space = {
             e = [
               ":sh rm -f /tmp/unique-file-h21a434"
