bef22f35340581ebeac4e148b7f8348c41891045
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-28 13:19:55 -0400

    nushell aliases

--- modules/homeManagerModules/nushell.nix
+++ modules/homeManagerModules/nushell.nix
@@ -44,6 +44,12 @@
       '';
       shellAliases = {
         h = "hx";
+        fr = "nh os switch --hostname magic /home/jr/flakes";
+        ft = "nh os test --hostname magic /home/jr/flakes";
+        zd = "zeditor";
+        ncg =
+          "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+
       };
     };
     carapace.enable = true;
