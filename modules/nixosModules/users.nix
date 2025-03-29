4ee114f2b093fbd50655dd2f5dd18f19936996b8
TSawyer87 <sawyerjr.25@gmail.com>
2025-03-29 09:12:24 -0400

    shell.nu

--- modules/nixosModules/users.nix
+++ modules/nixosModules/users.nix
@@ -1,14 +1,15 @@
-{
-  pkgs,
-  username,
-  lib,
-  config,
-  inputs,
-  ...
-}: let
+{ pkgs
+, username
+, lib
+, config
+, inputs
+, ...
+}:
+let
   inherit (import ../../hosts/magic/variables.nix) gitUsername;
-in {
-  options = {users.enable = lib.mkEnableOption "Enables users module";};
+in
+{
+  options = { users.enable = lib.mkEnableOption "Enables users module"; };
 
   config = lib.mkIf config.users.enable {
     users.users = {
@@ -17,10 +18,11 @@ in {
         isNormalUser = true;
         description = "${gitUsername}";
         hashedPassword = "$6$hLxz1nh01PVcUQ6e$4o6tYrRxbRQQFRN3NSUMkPuwdRpOhNdp1s07TAYr2shcbdQUkYurHyk8Xp8FvjVPwr60N4NSPDmwUr6Nd5FD9.";
-        extraGroups = ["networkmanager" "wheel" "libvirtd" "scanner" "lp" "root" "jr"];
-        shell = pkgs.zsh;
+        extraGroups = [ "networkmanager" "wheel" "libvirtd" "scanner" "lp" "root" "jr" ];
+        # shell = pkgs.zsh;
+        shell = pkgs.nu;
         ignoreShellProgramCheck = true;
-        packages = with pkgs; [tealdeer zoxide mcfly tokei stow inputs.home-manager.packages.${pkgs.system}.default];
+        packages = with pkgs; [ tealdeer zoxide mcfly tokei stow inputs.home-manager.packages.${pkgs.system}.default ];
       };
       # "newuser" = {
       #   homeMode = "755";
