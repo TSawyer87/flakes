85d608394efb927663a6e0c911672f38e57bb190
TSawyer87 <sawyerjr.25@gmail.com>
2025-04-01 10:15:09 -0400

    refactor keys hypr

--- modules/homeManagerModules/hypr/keybinds.nix
+++ modules/homeManagerModules/hypr/keybinds.nix
@@ -1,73 +1,67 @@
-{ host, username, config, ... }:
-let
-  inherit (import ../../../hosts/${host}/variables.nix)
-    browser terminal extraMonitorSettings keyboardLayout;
-  # submap = resize;
-in
-{
+{ ... }: {
   wayland.windowManager.hyprland = {
+    "$mod" = "SUPER";
     settings = {
-
       bind = [
-        "$modifier,Return,exec,${terminal}"
-        "$modifier,T,exec,kitty"
+        "$mod,Return,exec,ghostty"
+        "$mod,T,exec,kitty"
         # "CONTROL,SPACE,exec,rofi-launcher"
-        "$modifier SHIFT,W,exec,web-search"
-        "$modifier ALT,W,exec,killall -9 wpaperd && wpaperd"
-        "$modifier SHIFT,N,exec,swaync-client -rs"
-        "$modifier SHIFT,N,exec,swaync-client -rs"
-        "$modifier,W,exec,${browser}"
-        "$modifier,F,exec,firefox"
-        "$modifier,V,exec,cliphist list | wofi --dmenu | cliphist decode | wl-copy"
-        "$modifier,E,exec,emopicker9000"
-        "$modifier,S,exec,screenshootin"
-        "$modifier,D,exec,pkill wofi || wofi --normal-window --show drun --allow-images"
-        # "$modifier,O,exec,obs"
-        "$modifier,C,exec,hyprpicker -a"
-        "$modifier,G,exec,gimp"
-        "$modifier SHIFT,Return, exec,pypr toggle term"
-        "$modifier SHIFT,T,exec,pypr toggle thunar"
+        "$mod SHIFT,W,exec,web-search"
+        "$mod ALT,W,exec,killall -9 wpaperd && wpaperd"
+        "$mod SHIFT,N,exec,swaync-client -rs"
+        "$mod SHIFT,N,exec,swaync-client -rs"
+        "$mod,W,exec,firefox"
+        "$mod,F,exec,firefox"
+        "$mod,V,exec,cliphist list | wofi --dmenu | cliphist decode | wl-copy"
+        "$mod,E,exec,emopicker9000"
+        "$mod,S,exec,screenshootin"
+        "$mod,D,exec,pkill wofi || wofi --normal-window --show drun --allow-images"
+        # "$mod,O,exec,obs"
+        "$mod,C,exec,hyprpicker -a"
+        "$mod,G,exec,gimp"
+        "$mod SHIFT,Return, exec,pypr toggle term"
+        "$mod SHIFT,T,exec,pypr toggle thunar"
         "CONTROL ALT, Delete, exec, wlogout"
-        "$modifier,M,exec,pavucontrol"
-        "$modifier,Q,killactive,"
-        "$modifier,P,exec,pypr toggle volume"
-        "$modifier SHIFT,P,pseudo,"
-        "$modifier SHIFT,I,togglesplit,"
+        "$mod,M,exec,pavucontrol"
+        "$mod,Q,killactive,"
+        "$mod,P,exec,pypr toggle volume"
+        "$mod SHIFT,P,pseudo,"
+        "$mod SHIFT,I,togglesplit,"
         "ALT,Return,fullscreen,"
-        "$modifier SHIFT,F,togglefloating,"
-        "$modifier SHIFT,C,exit,"
-        "$modifier SHIFT,left,movewindow,l"
-        "$modifier SHIFT,right,movewindow,r"
-        "$modifier SHIFT,up,movewindow,u"
-        "$modifier SHIFT,down,movewindow,d"
-        "$modifier SHIFT,h,movewindow,l"
-        "$modifier SHIFT,l,movewindow,r"
-        "$modifier SHIFT,k,movewindow,u"
-        "$modifier SHIFT,j,movewindow,d"
-        "$modifier ALT, left, swapwindow,l"
-        "$modifier ALT, right, swapwindow,r"
-        "$modifier ALT, up, swapwindow,u"
-        "$modifier ALT, down, swapwindow,d"
-        "$modifier ALT, 43, swapwindow,l"
-        "$modifier ALT, 46, swapwindow,r"
-        "$modifier ALT, 45, swapwindow,u"
-        "$modifier ALT, 44, swapwindow,d"
-        "$modifier,left,movefocus,l"
-        "$modifier,right,movefocus,r"
-        "$modifier,up,movefocus,u"
-        "$modifier,down,movefocus,d"
-        "$modifier,h,movefocus,l"
-        "$modifier,l,movefocus,r"
-        "$modifier,k,movefocus,u"
-        "$modifier,j,movefocus,d"
-        "$modifier,0,workspace,10"
-        "$modifier SHIFT,SPACE,movetoworkspace,special"
-        "$modifier,SPACE,togglespecialworkspace"
-        "$modifier SHIFT,0,movetoworkspace,10"
-        "$modifier CONTROL,right,workspace,e+1"
-        "$modifier CONTROL,left,workspace,e-1"
-        "$modifier,mouse_down,workspace, e+1"
-        "$modifier,mouse_up,workspace, e-1"
+        "$mod SHIFT,F,togglefloating,"
+        "$mod SHIFT,C,exit,"
+        "$mod SHIFT,left,movewindow,l"
+        "$mod SHIFT,right,movewindow,r"
+        "$mod SHIFT,up,movewindow,u"
+        "$mod SHIFT,down,movewindow,d"
+        "$mod SHIFT,h,movewindow,l"
+        "$mod SHIFT,l,movewindow,r"
+        "$mod SHIFT,k,movewindow,u"
+        "$mod SHIFT,j,movewindow,d"
+        "$mod ALT, left, swapwindow,l"
+        "$mod ALT, right, swapwindow,r"
+        "$mod ALT, up, swapwindow,u"
+        "$mod ALT, down, swapwindow,d"
+        "$mod ALT, 43, swapwindow,l"
+        "$mod ALT, 46, swapwindow,r"
+        "$mod ALT, 45, swapwindow,u"
+        "$mod ALT, 44, swapwindow,d"
+        "$mod,left,movefocus,l"
+        "$mod,right,movefocus,r"
+        "$mod,up,movefocus,u"
+        "$mod,down,movefocus,d"
+        "$mod,h,movefocus,l"
+        "$mod,l,movefocus,r"
+        "$mod,k,movefocus,u"
+        "$mod,j,movefocus,d"
+        "$mod,0,workspace,10"
+        "$mod SHIFT,SPACE,movetoworkspace,special"
+        "$mod,SPACE,togglespecialworkspace"
+        "$mod SHIFT,0,movetoworkspace,10"
+        "$mod CONTROL,right,workspace,e+1"
+        "$mod CONTROL,left,workspace,e-1"
+        "$mod,mouse_down,workspace, e+1"
+        "$mod,mouse_up,workspace, e-1"
         "ALT,Tab,cyclenext"
         "ALT,Tab,bringactivetotop"
         ",XF86AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
@@ -79,50 +73,29 @@ in
         ",XF86AudioPrev, exec, playerctl previous"
         ",XF86MonBrightnessDown,exec,brightnessctl set 5%-"
         ",XF86MonBrightnessUp,exec,brightnessctl set +5%"
-        # "$modifier,R,submap,resize"
+        # "$mod,R,submap,resize"
       ] ++ (
         # workspaces
         # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-        builtins.concatLists (builtins.genList
-          (i:
-            let ws = i + 1;
-            in [
-              "$modifier, code:1${toString i}, workspace, ${toString ws}"
-              "$modifier SHIFT, code:1${toString i}, movetoworkspace, ${
-              toString ws
-            }"
-            ]) 9)) ++ (
-        # workspaces
-        # binds $mod + {1..9} to [to] workspace {1..9}
-        builtins.concatLists (builtins.genList
-          (i:
-            let ws = i + 1;
-            in [
-              "$modifier, code:1${toString i}, workspace, ${toString ws}"
-              "$modifier, code:1${toString i}, workspace, ${toString ws}"
-            ]) 9));
-      # binde = [
-      #   # "submap=resize"
-      #   ",right,resizeactive,50 0"
-      #   ",L,resizeactive,50 0"
-      #   ",left,resizeactive,-50 0"
-      #   ",H,resizeactive,-50 0"
-      #   ",up,resizeactive,0 -50"
-      #   ",K,resizeactive,0 -50"
-      #   ",down,resizeactive,0 50"
-      #   ",J,resizeactive,0 50"
-      #   "submap=reset"
-      # ];
+        builtins.concatLists (builtins.genList (i:
+          let ws = i + 1;
+          in [
+            "$mod, code:1${toString i}, workspace, ${toString ws}"
+            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+          ]) 9)) ++ (
+            # workspaces
+            # binds $mod + {1..9} to [to] workspace {1..9}
+            builtins.concatLists (builtins.genList (i:
+              let ws = i + 1;
+              in [
+                "$mod, code:1${toString i}, workspace, ${toString ws}"
+                "$mod, code:1${toString i}, workspace, ${toString ws}"
+              ]) 9));
 
-      bindm = [
-        "$modifier, mouse:272, movewindow"
-        "$modifier, mouse:273, resizewindow"
-      ];
+      bindm = [ "$mod, mouse:272, movewindow" "$mod, mouse:273, resizewindow" ];
 
     };
 
-    # extraConfig =
-    #   "\n      monitor=,preferred,auto,auto\n      monitor=DP-1, 3840x2160, 0x0, 1.5\n      ${extraMonitorSettings}\n    ";
   };
 }
 
--- modules/homeManagerModules/hypr/settings.nix
+++ modules/homeManagerModules/hypr/settings.nix
@@ -2,24 +2,7 @@
 {
   wayland.windowManager.hyprland = {
     settings = {
-      # exec-once = [
-      #   "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-      #   "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-      #   # "killall -q swww;sleep .5 && swww init"
-      #   "killall -q waybar;sleep .5 && waybar"
-      #   # "killall -q swaync;sleep .5 && swaync"
-      #   "nm-applet --indicator"
-      #   "lxqt-policykit-agent"
-      #   "pypr &"
-      #   # "sleep 1.5 && swww img /home/${username}/Pictures/Wallpapers/"
-      #   "wpaperd"
-      #   "killall -q mako;sleep .5 && mako"
-      #   "wl-paste --type text --watch cliphist store" # Stores only text data
-      #   "wl-paste --type image --watch cliphist store" # Stores only image data
-      # ];
-
       input = {
-        # kb_layout = "${keyboardLayout}";
         kb_layout = "us";
         kb_options = [ "grp:alt_caps_toggle" "caps:super" ];
         numlock_by_default = true;
@@ -35,7 +18,7 @@
       };
 
       general = {
-        "$modifier" = "SUPER";
+        # "$modifier" = "SUPER";
         layout = "dwindle";
         gaps_in = 4;
         gaps_out = 5;
@@ -194,7 +177,6 @@
     extraConfig = ''
       monitor=DP-1, 3840x2160, 0x0, 1.5
     '';
-    # "\n      monitor=,preferred,auto,auto\n      monitor=DP-1, 3840x2160, 0x0, 1.5\n      ${extraMonitorSettings}\n    ";
   };
 }
 
