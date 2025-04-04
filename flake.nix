e4cf59431423d86195f03a993aa2c0c9c532790c
TSawyer87 <sawyerjr.25@gmail.com>
2025-04-04 08:57:13 -0400

    wallpapers

--- flake.nix
+++ flake.nix
@@ -26,13 +26,13 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     yazi.url = "github:sxyazi/yazi";
-    wallpapers = {
-      url = "github:TSawyer87/wallpapers";
-      flake = false;
-    };
+    # wallpapers = {
+    #   url = "github:TSawyer87/wallpapers";
+    #   flake = false;
+    # };
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-index-database, yazi, nvf, rose-pine-hyprcursor, wallpapers, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, nix-index-database, yazi, nvf, rose-pine-hyprcursor, ... }@inputs:
     let
       inherit (self) outputs;
 
