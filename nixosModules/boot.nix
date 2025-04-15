f3ada678e79f605697560464679dcdb85f8d1285
TSawyer87 <sawyerjr.25@gmail.com>
2025-04-15 12:06:22 -0400

    zen-kernel

--- nixosModules/boot.nix
+++ nixosModules/boot.nix
@@ -1,31 +1,12 @@
 {
-  config,
-  lib,
+  # config,
+  # lib,
   pkgs,
   ...
-}: let
-  zfsCompatibleKernelPackages =
-    lib.filterAttrs (
-      name: kernelPackages:
-        (builtins.match "linux_[0-9]+_[0-9]+" name)
-        != null
-        && (builtins.tryEval kernelPackages).success
-        && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
-    )
-    pkgs.linuxKernel.packages;
-  latestKernelPackage = lib.last (
-    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
-      builtins.attrValues zfsCompatibleKernelPackages
-    )
-  );
-in {
+}: {
   boot = {
-    supportedFilesystems = ["zfs"];
-    zfs.forceImportRoot = false;
     # LinuxZen Kernel
-    # kernelPackages = pkgs.linuxPackages_zen;
-    # Note this might jump back and forth as kernels are added or removed.
-    kernelPackages = latestKernelPackage;
+    kernelPackages = pkgs.linuxPackages_zen;
     consoleLogLevel = 3;
     # disable wifi powersave
     extraModprobeConfig = ''
@@ -47,19 +28,7 @@ in {
       };
     };
     plymouth.enable = true;
-    # kernelModules = ["v4l2loopback"];
-    # extraModulePackages = [config.boot.kernelPackages.v4l2loopback];
-    # Appimage Support
-    # binfmt.registrations.appimage = {
-    #   wrapInterpreterInShell = false;
-    #   interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-    #   recognitionType = "magic";
-    #   offset = 0;
-    #   mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
-    #   magicOrExtension = "\\x7fELF....AI\\x02";
-    # };
   };
-  networking.hostId = "2d393b76";
   environment.systemPackages = with pkgs; [
     greetd.tuigreet
   ];
